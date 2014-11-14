c07663d149173078c00db09e24bf9cf43ce62693
Evan <evturn>
2014-11-13 21:45:02 -0500

    changed evturn to evan turner

---
+++ javascripts/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  
+  // $(".about").hide();
+
+  // $(".active").mouseover(function(){
+  //  $(".about").stop().slideDown("slow");
+  // });
+  // $(".active").mouseout(function(){
+  //  $(".about").slideUp("slow");
+  // });
+ });
