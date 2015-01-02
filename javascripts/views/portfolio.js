d0484ea92aa5465f8a6e2b42307d57ae6810f822
Evan Turner <evturn@gmail.com>
2015-01-02 03:03:52 -0500

    portfolio view created

--- javascripts/views/portfolio.js
+++ javascripts/views/portfolio.js
@@ -1,3 +1,13 @@
+var Portfolio = Backbone.View.extend({
+	el: $('#portfolio'),
+	template: _.template($('#portfolio-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.template());
+	}
+});
 
 
 console.log('Portfolio');
