5f6dd7210284da8a1967944e4884ead210f9f975
Evan Turner <evturn@gmail.com>
2015-01-02 08:19:42 -0500

    stack view created

--- javascripts/views/stack.js
+++ javascripts/views/stack.js
@@ -1,1 +1,13 @@
+var Stack = Backbone.View.extend({
+	el: $('#stack'),
+	template: _.template($('#stack-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.template());
+		return this;
+	}
+});
+
 console.log('Stack');
