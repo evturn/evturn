143bf9cf81c643a0f2e153a07461b1ec77f3e721
Evan Turner <evturn@gmail.com>
2015-01-01 22:43:50 -0500

    bioview created

--- javascripts/backbone/views/bio.js
+++ javascripts/backbone/views/bio.js
@@ -1,3 +1,10 @@
 var BioView = Backbone.View.extend({
-
+	el: $('#bio'),
+	initialize: function() {
+	template: _.bioTemplate($('#bio-template').html()),
+		this.render()
+	},
+	render: function() {
+		this.$el.html(this.template());
+	}
 });
