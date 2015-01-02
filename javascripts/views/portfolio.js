15c27658b45ad82f7f28941cd321138f2171d0f7
Evan Turner <evturn@gmail.com>
2015-01-02 03:50:10 -0500

    project view created

--- javascripts/views/portfolio.js
+++ javascripts/views/portfolio.js
@@ -1,7 +1,6 @@
 var Portfolio = Backbone.View.extend({
 	el: $('#portfolio'),
 	tabsTemplate: _.template($('#portfolio-template').html()),
-	projectTemplate: _.template($('#project-template').html()),
 	initialize: function() {
 		this.render();
 	},
--- javascripts/views/project.js
+++ javascripts/views/project.js
@@ -1,1 +1,13 @@
+var ProjectView = Backbone.View.extend({
+	el: $('#project'),
+	template: _.template($('#project-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.template());
+		return this;
+	}
+});
+
 console.log('ProjectView');
