2561c8bc3e5821a59cb20ed49235e6fefd5f8f37
Evan Turner <evturn@gmail.com>
2015-01-02 00:28:08 -0500

    contact view created

--- javascripts/views/contact.js
+++ javascripts/views/contact.js
@@ -1,6 +1,13 @@
 var Contact = Backbone.View.extend({
 	el: $('#contact'),
-	
+	template: _.template($('#contact-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.template());
+		return this;
+	}
 });
 
 
