2500adae9261872a1e0b30f0eb7407e76fba48a1
Evan Turner <evturn@gmail.com>
2015-02-11 15:24:11 -0500

    main content on landing page rendering in one view

--- assets/js/views/landingView.js
+++ assets/js/views/landingView.js
@@ -1,15 +1,27 @@
 var LandingView = Backbone.View.extend({
 	el: '#wrapper',
 	bioTemplate: _.template($('#bio-template').html()),
+	stackTemplate: _.template($('#stack-template').html()),
+	avatarTemplate: _.template($('#avatar-template').html()),
+	contactTemplate: _.template($('#contact-template').html()),
 	initialize: function() {
 		this.bioView();
-		new Stack();
-		new Avatar();
-		new Contact();
+		this.stackView();
+		this.avatarView();
+		this.contactView();
 		new Portfolio();
 		new MarshallzView();
 	},
 	bioView: function() {
 		$('#bio').html(this.bioTemplate);
+	},
+	stackView: function() {
+		$('#stack').html(this.stackTemplate);
+	},
+	avatarView: function() {
+		$('#avatar').html(this.avatarTemplate);
+	},
+	contactView: function() {
+		$('#contact').html(this.contactTemplate);
 	},
 });
