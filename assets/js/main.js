bb44601476a09cb65dd4bdec3090d11dc25a1082
Evan Turner <evturn@gmail.com>
2015-02-12 21:53:59 -0500

    producing all photos but in a line

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ new AppView();
 
 projectsCollection = new ProjectsCollection(projects);
 
+projectImages = projectsCollection.models[0].get('image');
+
+
 $projectEl = $('#project-summary');
 
 portfolioView  = new PortfolioView();
@@ -11,3 +14,7 @@ $('.footnote').hide();
 $('.carousel').carousel({
 	interval: 2000
 });
+
+
+
+
--- data.js
+++ data.js
@@ -3,10 +3,13 @@ projects = [
 		name: 'Blog',
 		summary: "I originally built this blog in Sinatra. I implemented user authentication using bcrypt's hash algorithm and used PostgreSQL to store encrypted passwords. Once a user creates an account they are granted access to tools that allow them to create new posts as well as edit or delete any of their published entries. I recently revisted this project and rebuilt it using Node on the server-side, Backbone for the client, and MongoDB for storage. Since I never used the original blog, I decided to write a task that uses the New York Times API to select sentences at random from various recent articles. These sentences are then combined to create new, nonsensical blog posts which are authored by a fictious nine year old user. In addition, anyone wanting to leave comments on these posts can do so by signing in with their Facebook account",
 		stack: "Rebuilt with Node.js &#x95; Express.js &#x95; MongoDB &#x95; Backbone.js &#x95; Handlebars &#x95; Underscore.js &#x95; Javascript<br>(Originally Sinatra &#x95; Ruby &#x95; PostgreSQL)",
-		carousel1: 'assets/img/marshallz-1.png',
-		carousel2: 'assets/img/marshallz-2.png',
-		carousel3: 'assets/img/marshallz-3.png',
-		carousel4: 'assets/img/marshallz-4.png'
+		image: 
+			[
+			'assets/img/marshallz-1.png', 
+			'assets/img/marshallz-2.png', 
+			'assets/img/marshallz-3.png', 
+			'assets/img/marshallz-4.png'
+			]
 	},
 	{
 		name: 'BAC Calculator',
