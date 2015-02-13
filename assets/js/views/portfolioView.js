ce1aba040f3a74e611f4985c40d0d8400c6e37cd
Evan Turner <evturn@gmail.com>
2015-02-13 01:57:15 -0500

    hangman template removed from project

--- assets/js/views/portfolioView.js
+++ assets/js/views/portfolioView.js
@@ -2,7 +2,6 @@ var PortfolioView = Backbone.View.extend({
 	el: '#portfolio',
 	projectTemplate: _.template($('#project-template').html()),
 	projectsNavTemplate: _.template($('#projects-nav-template').html()),
-	hangmanTemplate: _.template($('#hangman-template').html()),
 	tttTemplate: _.template($('#ttt-template').html()),
 	ramenBuffetTemplate: _.template($('#ramen-template').html()),
 	initialize: function() {
