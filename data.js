7538911cb6feda2986527404271945a4b503795d
Evan Turner <evturn@gmail.com>
2015-02-12 19:34:32 -0500

    gave each model a name attribute in preparation forconversion into a collection array

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 marshallzModel =  
 {
+	name: 'Blog',
 	summary: "I originally built this blog in Sinatra. I implemented user authentication using bcrypt's hash algorithm and used PostgreSQL to store encrypted passwords. Once a user creates an account they are granted access to tools that allow them to create new posts as well as edit or delete any of their published entries. I recently revisted this project and rebuilt it using Node on the server-side, Backbone for the client, and MongoDB for storage. Since I never used the original blog, I decided to write a task that uses the New York Times API to select sentences at random from various recent articles. These sentences are then combined to create new, nonsensical blog posts which are authored by a fictious nine year old user. In addition, anyone wanting to leave comments on these posts can do so by signing in with their Facebook account",
 	stack: "Rebuilt with Node.js &#x95; Express.js &#x95; MongoDB &#x95; Backbone.js &#x95; Handlebars &#x95; Underscore.js &#x95; Javascript<br>(Originally Sinatra &#x95; Ruby &#x95; PostgreSQL)",
 	carousel1: 'assets/img/marshallz-1.png',
@@ -10,6 +11,7 @@ marshallzModel =
 
 alculatorModel = 
 	{
+		name: 'BAC Calculator',
 		summary: "Users select items from an inventory of alcoholic beverages and have access to a beer database. These selection(s) along with the user's information return a blood alcohol level, a description, and color scheme representing stages from low traces of alcohol to lethal doses",
 		stack: "Rebuilt with Node.js &#x95; Express.js &#x95; Backbone.js &#x95; Underscore.js &#x95; Javascript<br>(Originally Rails &#x95; Backbone.js &#x95; Underscore.js &#x95; Ruby &#x95; Javascript)",
 		carousel1: "assets/img/alculator-1.png",
@@ -19,6 +21,7 @@ alculatorModel =
 
 hangmanModel = 
 	{
+		name: 'Hangman',
 		summary: "Hangman was original built in Sinatra using Ruby. The game state updated in real time and was stored using a PostgreSQL database. Months later I revisited the project for fun and remade the game from scratch in Javascript Backbone.js handling the views as the game is played",
 		stack: "Rebuilt with Node.js &#x95; Express.js &#x95; Backbone.js &#x95; Underscore.js &#x95; Javascript<br>(Originally Sinatra &#x95; Ruby &#x95; PostgreSQL &#x95; AJAX)",
 		carousel1: "assets/img/hangman-2.png",
@@ -29,6 +32,7 @@ hangmanModel =
 
 tttModel = 
 	{
+		name: 'Tic Tac Toe',
 		summary: "This version of Tic Tac Toe was built using Backbone.js. I sparingly used jQuery for this project giving the entire workload of event handling to my Backbone Views. I designed images for each game piece with Adobe Photoshop. Each image's file path was stored as an attribute to a certain model within the application and then used to render bits and pieces of the game in the browser on initial page load and during gameplay",
 		stack: "Built with Backbone.js &#x95; Underscore.js &#x95; Javascript",
 		image: 'assets/img/ttt.png'
@@ -36,6 +40,7 @@ tttModel =
 
 ramenBuffetModel = 
 	{
+		name: 'To-Do List',
 		summary: "This To-Do list requires no refreshing and saves instantly upon click or keydown. Data is stored locally on your machine with Backbone.localStorage", 
 		stack: "Built with Backbone.js &#x95; Underscore.js &#x95; Backbone.localStorage &#x95; Javascript",
 		image: 'assets/img/ramen-buffet.png',
