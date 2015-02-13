d6b5925215c64f45df09dc0619b89653c95a67b2
Evan Turner <evturn@gmail.com>
2015-02-12 19:19:09 -0500

    all images removed from html

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ tttSummary =
 	{
 		summary: "This version of Tic Tac Toe was built using Backbone.js. I sparingly used jQuery for this project giving the entire workload of event handling to my Backbone Views. I designed images for each game piece with Adobe Photoshop. Each image's file path was stored as an attribute to a certain model within the application and then used to render bits and pieces of the game in the browser on initial page load and during gameplay",
 		stack: "Built with Backbone.js &#x95; Underscore.js &#x95; Javascript",
-
+		image: 'assets/img/ttt.png'
 	}
 
 ramenBuffetSummary = 
