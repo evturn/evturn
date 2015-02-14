e3f1a30ef4dd6e7a40ea21949ca285f96b99f28a
Evan Turner <evturn@gmail.com>
2015-02-14 00:18:30 -0500

    moved button creator function into portfolio view

--- assets/js/views/portfolioView.js
+++ assets/js/views/portfolioView.js
@@ -49,13 +49,13 @@ var PortfolioView = Backbone.View.extend({
 		$projectEl.html(this.projectTemplate(model5.toJSON()));
 	},
 	createId: function(string) {
-  counter = 1;
-  for (counter + 1; counter <= total; counter++) {
-    var projectButton = new ProjectButton;
-    buttonId = string + '-' + counter;
-    projectNumber = counter;
-    projectButton.set({number: projectNumber, id: buttonId});
-    buttons.push(projectButton);
-  };
-},
+	  counter = 1;
+	  for (counter + 1; counter <= total; counter++) {
+	    var projectButton = new ProjectButton;
+	    buttonId = string + '-' + counter;
+	    projectNumber = counter;
+	    projectButton.set({number: projectNumber, id: buttonId});
+	    buttons.push(projectButton);
+	  };
+	},
 });
