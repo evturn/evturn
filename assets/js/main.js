e4ac8f1e15133b739aa8fffd201835b2e65d8ff6
Evan Turner <evturn@gmail.com>
2015-02-13 23:15:55 -0500

    button creator function working

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,13 @@ $('.carousel').carousel({
 	interval: 2000
 });
 
-
-
+newIds = [];
+function createId(string) {
+  counter = 1;
+  for (counter + 1; counter <= 3; counter++) {
+    elementId = string + '-' + counter;
+    newIds.push(elementId);
+  };
+};
 
 
