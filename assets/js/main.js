a2f904d5863d40294aae556db68697846256c04b
Evan Turner <evturn@gmail.com>
2015-03-04 15:06:36 -0500

    rewrote bio and broke it into more paragraphs

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,14 @@ $(function() {
 	$('#tooltip').tooltip()
 });
 
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 1) {
-            $('.dmtop').css({bottom:"25px"});
-        } else {
-            $('.dmtop').css({bottom:"-100px"});
-        }
-    });
-    $('.dmtop').click(function(){
-        $('html, body').animate({scrollTop: '0px'}, 800);
-        return false;
-    });
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 1) {
+  	$('.dmtop').css({bottom:"25px"});
+  } else {
+    $('.dmtop').css({bottom:"-100px"});
+  }
+});
+$('.dmtop').click(function(){
+  $('html, body').animate({scrollTop: '0px'}, 800);
+  return false;
+});
