b6221217c7971d04a83ad6f26cdbe65cb647290b
Evan Turner <evturn@gmail.com>
2015-03-05 17:14:07 -0500

    update to bio

--- data.js
+++ data.js
@@ -85,7 +85,7 @@ links = [
 
 copy = {
 	lead: 'As the web continues to evolve in the direction of single page applications, exploring solutions and strategies for building these rich front-end apps is not only essential but provides an exciting opportunity for design innovation.',
-	paragraph1: 'As a JavaScript Developer, I focus on building responsive single page applications that optimize scalability through restful backends. Although I initially built apps almost exclusively in a Rails environment with Ruby, my attraction to the world of isomorphic JavaScript has heavily strengthened the application logic I write.',
+	paragraph1: 'As a JavaScript Developer, I focus on building responsive single page applications that optimize scalability through RESTful backends. Although I initially built apps almost exclusively in a Rails environment with Ruby, my attraction to the world of isomorphic JavaScript has heavily strengthened the application logic I write.',
 	paragraph2: 'Given my familiarity with the Model–view–controller (MVC) architectural pattern that Rails implements, I find employing JavaScript libraries like Backbone.js or AngularJS a pertinent fit on the frontend.',
 	fact: '<strike>Evan has built applications since the age of 3. He also invented the internet</strike><sup><a id="footnote" href="#">[1]</a></sup>',
 	footnote: '<em>Evan did not invent the internet</em><sup> 1</sub>',
