17630c210f6381541387fe5cad6ce763cbdd86cd
Evan Turner <evturn@gmail.com>
2015-03-11 19:53:36 -0400

    one function to load all projects and make click events choose each model of the project to display

--- assets/js/views/portfolioView.js
+++ assets/js/views/portfolioView.js
@@ -2,15 +2,10 @@ var PortfolioView = Backbone.View.extend({
 	el: '#portfolio',
 	initialize: function() {
 		this.addAll();
-		this.setProject1();
+		// this.initializeCarousel();
 	},
 	events: {
-		'click #button-1': 'setProject1',
-		'click #button-2': 'setProject2',
-		'click #button-3': 'setProject3',
-		'click #button-4': 'setProject4',
-		'click #button-5': 'setProject5',
-    'click #button-6': 'setProject6'
+		'click a' : 'setProject'
 	},
   addOne: function(model) {
     var view = new ProjectView({model: model});
@@ -23,30 +18,16 @@ var PortfolioView = Backbone.View.extend({
       this.addOne(model);
     }.bind(this));
   },
-  setProject1: function() {
-  	var project = this.collection.get(1);
-  	this.addOne(project);
-  },
-  setProject2: function() {
-  	var project = this.collection.get(2);
-  	this.addOne(project);
-  },
-  setProject3: function() {
-  	var project = this.collection.get(3);
-  	this.addOne(project);
-  },
-  setProject4: function() {
-  	var project = this.collection.get(4);
-  	this.addOne(project);
-  },
-  setProject5: function() {
-  	var project = this.collection.get(5);
-  	this.addOne(project);
-  },
-  setProject6: function() {
-    var project = this.collection.get(6);
+  setProject: function(e) {
+    e.preventDefault();
+    var id = $(e.currentTarget).data('id');
+    var project = this.collection.get(id);
     this.addOne(project);
   },
+  initializeCarousel: function() {
+  	var project = this.collection.get(1);
+  	this.addOne(project);
+ },
 	activateCarousel: function() {
 		$('.carousel').carousel({
 			interval: 3500
