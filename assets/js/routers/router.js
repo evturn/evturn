be27d909480d518dc1143586fddae8bf47842a2c
Evan Turner <evturn@gmail.com>
2015-03-12 04:10:48 -0400

    moved all document ready functions into router for now

--- assets/js/routers/router.js
+++ assets/js/routers/router.js
@@ -1,11 +1,37 @@
 var Router = Backbone.Router.extend({
 	routes: {
-		'' : 'index'
+		'' : 'egg'
 	},
 	initialize: function() {
 		new AppView();
+		this.egg();
 	},
-	index: function() {
-		$('#tooltip').tooltip()
+	egg: function() {
+
+		$(function() {
+			$('#tooltip').tooltip()
+
+			$(window).load(function() {
+				$("#preloader").delay(500).fadeOut();
+	  		$(".preloader").delay(600).fadeOut("slow");
+			});
+
+			$(window).scroll(function(){
+	  		if ($(this).scrollTop() > 1) {
+	  			$('.dmtop').css({bottom:"25px"});
+				} else {
+	    		$('.dmtop').css({bottom:"-100px"});
+	  		}
+			});
+
+			$('.dmtop').click(function(){
+	  		$('html, body').animate({scrollTop: '0px'}, 800);
+	  		return false;
+			});
+
+		});
+
 	},
 });
+
+router = new Router();
