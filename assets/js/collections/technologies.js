5bd86f2064c6ed42921718a06c19906e3c9add70
Evan Turner <evturn@gmail.com>
2015-04-12 09:52:36 -0400

    new icons brought in

--- assets/js/collections/technologies.js
+++ assets/js/collections/technologies.js
@@ -5,14 +5,67 @@ var Technologies = Backbone.Collection.extend({
 });
 
 var stacks = [
-				{technology: 'Express.js'},
-				{technology: 'Node.js'},
-				{technology: 'AngularJS'},
-				{technology: 'Backbone.js'},
+				{
+					technlogy: 'JavaScript',
+					icon: 'devicon-javascript-plain'
+				},
+				{
+					technology: 'Express.js',
+					icon: 'fa fa-diamond'
+				},
+				{
+					technology: 'Node.js',
+					icon: 'devicon-nodejs-plain'
+				},
+				{
+					technology: 'AngularJS',
+					icon: 'devicon-angularjs-plain'
+				},
+				{
+					technology: 'Backbone.js',
+					icon: 'devicon-backbonejs-plain'
+				},
+				{
+					technology: 'jQuery',
+					icon: 'devicon-jquery-plain'
+				},
 			 	{technology: 'Heroku'},
-			 	{technology: 'Digital Ocean'},
-			 	{technology: 'MongoDB'},
-			 	{technology: 'Firebase'}
+			 	{
+			 		technology: 'Digital Ocean',
+			 		icon: 'devicon-ubuntu-plain'
+			 	},
+			 	{
+			 		technology: 'MongoDB',
+			 		icon: 'devicon-mongodb-plain'
+			 	},
+			 	{
+			 		technology: 'Bootstrap',
+			 		icon: 'devicon-bootstrap-plain'
+				},
+				{
+				 	technology: 'git',
+				 	icon: 'devicon-git-plain'
+				},
+			 	{
+			 		technology: 'Firebase',
+			 		icon: 'fa fa-database'
+			 	},
+			 	{
+			 		technology: 'Photoshop',
+			 		icon: 'devicon-photoshop-plain'
+			 	},
+			 	{
+			 		technology: 'PostgreSQL',
+			 		icon: 'devicon-postgresql-plain'
+			 	},
+			 	{
+			 		technology: 'Rails',
+			 		icons: 'devicon-rails-plain'
+			 	},
+			 	{
+			 		technology: 'Ruby',
+			 		icon: 'devicon-ruby-plain'
+			 	}
 ];
 
 var technologies = new Technologies(stacks)
