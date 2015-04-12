cd63bbc3b056d6eb832897df0cf8b61e689db8c9
Evan Turner <evturn@gmail.com>
2015-04-11 21:37:46 -0400

    modified about section content

--- assets/js/models/about.js
+++ assets/js/models/about.js
@@ -2,7 +2,7 @@ var About = Backbone.Model.extend({});
 
 var bio = {
 	lead: 'As the web continues to evolve in the direction of single page applications, exploring solutions and strategies for building these rich front-end apps is not only essential but provides an exciting opportunity for design innovation.',
-	paragraph1: 'As a JavaScript Developer, I focus on building responsive single page applications that optimize scalability through RESTful APIs. I initially built apps almost exclusively in a Rails environment with Ruby, but my attraction to the world of isomorphic JavaScript has heavily strengthened the application logic I write.',
+	paragraph1: 'As a JavaScript Developer, I focus on building responsive single page applications that optimize scalability through RESTful APIs. I initially built apps almost exclusively in a Rails environment writing Ruby, but my attraction to the JavaScript language and the seemingly endless amount of libraries has heavily strengthened the application logic I write.',
 	paragraph2: 'Given my familiarity with the Model–view–controller (MVC) architectural pattern that Rails implements, I find employing JavaScript libraries like Backbone.js and AngularJS a pertinent fit on the frontend.'
 	};
 
