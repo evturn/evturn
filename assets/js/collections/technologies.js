3c3564139a60211629714a4922cc521dc67f8d8c
Evan Turner <evturn@gmail.com>
2015-04-12 09:03:54 -0400

    contact, code, and technologies all entered in same section

--- assets/js/collections/technologies.js
+++ assets/js/collections/technologies.js
@@ -5,46 +5,14 @@ var Technologies = Backbone.Collection.extend({
 });
 
 var stacks = [
-	{
-		image: 'assets/img/js-stack-white.png',
-		technologies: 
-			[
 				{technology: 'Express.js'},
 				{technology: 'Node.js'},
 				{technology: 'AngularJS'},
-				{technology: 'Backbone.js'}
-			]
-	},
-	{
-		image: 'assets/img/gear-stack-white.png',
-		technologies: 
-			[
-				{technology: 'Handlebars'},
-				{technology: 'Underscore.js'},
-				{technology: 'jQuery'},
-				{technology: 'Bootstrap'}
-			]
-	},
-	{
-		image: 'assets/img/grid-stack-white.png',
-		technologies: 
-			[
-			 {technology: 'Heroku'},
-			 {technology: 'Digital Ocean'},
-			 {technology: 'MongoDB'},
-			 {technology: 'Firebase'}
-			]
-	},
-	{
-		image: 'assets/img/rails-stack-white.png',
-		technologies: 
-			[
-			 {technology: 'Sinatra'},
-			 {technology: 'Rails'},
-			 {technology: 'PostgreSQL'}, 
-			 {technology: 'Git'}
-			]
-	}
+				{technology: 'Backbone.js'},
+			 	{technology: 'Heroku'},
+			 	{technology: 'Digital Ocean'},
+			 	{technology: 'MongoDB'},
+			 	{technology: 'Firebase'}
 ];
 
 var technologies = new Technologies(stacks)
--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,20 @@ $(function() {
 	});
 
 });
+
+    function count($this){
+        var current = parseInt($this.html(), 10);
+        current = current + 50;
+        $this.html(++current);
+            if(current > $this.data('count')){
+                $this.html($this.data('count'));
+            } 
+            else {    
+                setTimeout(function(){count($this)}, 50);
+            }
+        }        
+        $(".stat-count").each(function() {
+        $(this).data('count', parseInt($(this).html(), 10));
+        $(this).html('0');
+        count($(this));
+    });
