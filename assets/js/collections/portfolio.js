92ebe84cf77e3f5682798b6b3953236728debf5f
Evan Turner <evturn@gmail.com>
2015-04-12 12:19:53 -0400

    projects thrown into array to use or not on site

--- assets/js/collections/portfolio.js
+++ assets/js/collections/portfolio.js
@@ -2,7 +2,7 @@ var Portfolio = Backbone.Collection.extend({
 	model: Project,
 });
 
-var projects = [
+var piqueBeta =
 	{
 		name: 'Pique',
 		description: "Pique is an app for people who are interested in networking, collaborating, and working on projects. I built the front end of this site which allows potential new users to sign up before the app is released. Views and route parameters are all handled by Backbone. Data from the input fields are saved using Firebase.",
@@ -21,7 +21,8 @@ var projects = [
 				{item: 'assets/img/pique-3.jpg'}, 
 				{item: 'assets/img/pique-4.jpg'}
 			]
-	}, 
+	};
+var musicInsider =
 	{
 		name: 'Music Insider',
 		description: "Music Insider is a Los Angeles based podcast. Using Backbone.js I designed an Admin interface that uses Firebase for User Authentication and real time cloud storage. Clients that are authenticated are given access to admin tools to where the user can add and update posts.",
@@ -42,7 +43,8 @@ var projects = [
 				{item: 'assets/img/mi-4.jpg'},
 				{item: 'assets/img/mi-5.jpg'}
 			]
-	},
+	};
+var marshallz =
 	{
 		name: 'Blog',
 		description: "All blog posts are created using the New York Times API to select sentences at random from various articles. These sentences are then combined to create new, nonsensical blog posts which are authored by a fictious nine year old user. In addition, these posts allow comments from other users by signing in with their Facebook or Twitter account",
@@ -61,7 +63,8 @@ var projects = [
 				{item: 'assets/img/marshallz-3.jpg'}, 
 				{item: 'assets/img/marshallz-4.jpg'}
 			]
-	},
+	};
+var alculator =
 	{
 		name: 'BAC Calculator',
 		description: "Users select from an investory of alcoholic beverages. In addition, users have search access to a database of beer. All alcohol percentages for the user's selected beverages gathered. That data, along with the user's information, is run through the Widmark formula to calculate the individual's blood alcohol level. The results provide a description particular to the user's intoxication level ranging from low traces of alcohol to lethal doses.",
@@ -80,7 +83,8 @@ var projects = [
 				{item: "assets/img/alculator-1.png"},
 				{item: "assets/img/alculator-4.png"}
 			]
-	},
+	};
+var ramenBuffet =
 	{
 		name: 'Task Manager',
 		description: "This single page AngularJS app  stores the data from the user input using MongoDB. Users can create an account for the app and in addition login and link their Twitter and Facebook accounts. I have built this app multiple times since the concept provides a great avenue to exploring other technologies. Other iterations of this app used Backbone.js, Marionette.js, and Firebase.", 
@@ -98,7 +102,8 @@ var projects = [
 				{item: "assets/img/ramen-3.jpg"},
 				{item: "assets/img/ramen-2.jpg"}
 			]
-	},
+	};
+var hangman =
 	{
 		name: 'Hangman',
 		description: "Hangman was original built in Sinatra using Ruby. The game state updated in real time and was stored using a PostgreSQL database. Months later I remade the game from with Backbone.js",
@@ -117,7 +122,8 @@ var projects = [
 				{item: "assets/img/hangman-3.jpg"},
 				{item: "assets/img/hangman-4.jpg"}
 			]
-	}, 
+	};
+var tictactoe =
 	{
 		name: 'Tic Tac Toe',
 		description: "This version of Tic Tac Toe was built using Backbone.js. I sparingly used jQuery for this project giving the entire workload of event handling to my Backbone Views. I designed images for each game piece with Adobe Photoshop. Each image's file path was stored as an attribute to a certain model within the application and then used to render bits and pieces of the game in the browser on initial page load and during gameplay",
@@ -131,8 +137,8 @@ var projects = [
 			[
 				{item: 'assets/img/ttt-2.jpg'}
 			]
-	}
+	};
 
-];
+var projects = [piqueBeta, musicInsider, marshallz, alculator, ramenBuffet, hangman, tictactoe];
 
 var portfolio  = new Portfolio(projects);
--- assets/js/collections/technologies.js
+++ assets/js/collections/technologies.js
@@ -16,48 +16,54 @@ var angular =
 		icon: 'devicon-angularjs-plain',
 		id: 2
 	};
-var backbone =  		{
-											technology: 'Backbone.js',
-											icon: 'devicon-backbonejs-plain',
-											id: 3
-										};
-var jQuery = 				{
-											technology: 'jQuery',
-											icon: 'devicon-jquery-plain',
-											id: 4
-										};
-var bootstrap =  		{
-											technology: 'Bootstrap',
-									 		icon: 'devicon-bootstrap-plain',
-									 		id: 5
-										};
-var git =						{
-										 	technology: 'git',
-										 	icon: 'devicon-git-plain',
-										 	id: 6
-										};
-var photoshop =			{
-									 		technology: 'Photoshop',
-									 		icon: 'devicon-photoshop-plain',
-									 		id: 7
-									 	};
-var digitalOcean =	{
-						 					technology: 'Digital Ocean',
-						 					icon: 'devicon-ubuntu-plain',
-						 					id: 8
-						 				};
-var firebase =      {
-											technology: 'Firebase',
-											icon: 'fa fa-database fa-3x',
-											id: 9
-										};
+var backbone =  		
+	{
+		technology: 'Backbone.js',
+		icon: 'devicon-backbonejs-plain',
+		id: 3
+	};
+var jQuery = 				
+	{
+		technology: 'jQuery',
+		icon: 'devicon-jquery-plain',
+		id: 4
+	};
+var bootstrap =  		
+	{
+		technology: 'Bootstrap',
+		icon: 'devicon-bootstrap-plain',
+		id: 5
+	};
+var git =						
+	{
+	 	technology: 'git',
+	 	icon: 'devicon-git-plain',
+	 	id: 6
+	};
+var photoshop =			
+	{
+		technology: 'Photoshop',
+		icon: 'devicon-photoshop-plain',
+		id: 7
+ 	};
+var digitalOcean =	
+	{
+		technology: 'Digital Ocean',
+		icon: 'devicon-ubuntu-plain',
+		id: 8
+	};
+var firebase =      
+	{
+		technology: 'Firebase',
+		icon: 'fa fa-database fa-3x',
+		id: 9
+	};
 var mongoDB =
 	{
 		technology: 'MongoDB',
 		icon: 'devicon-mongodb-plain',
 		id: 10
 	};								
-
 
 var stacks = [node, angular, backbone, jQuery, bootstrap, git, photoshop, digitalOcean];
 
