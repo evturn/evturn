e17cece84df2d0066f23cdc4023b778eb38d6dc4
Evan Turner <evturn@gmail.com>
2015-04-21 04:15:04 -0400

    event handler added for contact in navbar

--- assets/js/views/navbar.js
+++ assets/js/views/navbar.js
@@ -1,13 +1,17 @@
 var Navbar = Backbone.View.extend({
 	el: '.site-navigation',
 	events: {
-		'click .nav-home'  : 'home',
-		'click .nav-about' : 'about'
+		'click .nav-home'  	 : 'home',
+		'click .nav-about' 	 : 'about',
+		'click .nav-contact' : 'contact'
 	},
 	home: function() {
 		router.navigate('', {trigger: true});
 	},
 	about: function() {
 		router.navigate('about', {trigger: true});
+	},
+	contact: function() {
+		router.navigate('contact', {trigger: true});
 	},
 });
