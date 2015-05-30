49208136ff12b9e19a9f356a58d6a66b4a6d20a7
Evan Turner <evturn@gmail.com>
2015-05-30 17:34:34 -0400

    reorganized script functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,20 @@
+var router = new Router();
+Backbone.history.start();
 
 function count($this){
-    var current = parseInt($this.html(), 10);
-    current = current + 50;
-    $this.html(++current);
-    if(current > $this.data('count')){
-      $this.html($this.data('count'));
-    } 
-    else {    
-      setTimeout(function(){count($this)}, 50);
-    }
+  var current = parseInt($this.html(), 10);
+  current = current + 50;
+  $this.html(++current);
+  if (current > $this.data('count')) {
+    $this.html($this.data('count'));
+  } else {    
+    setTimeout(function() {
+      count($this)
+      }, 50);
   }
-
-/* ==============================================
-    Animations
-=============================================== */
+}
 
 new WOW().init();
-
 
 $(function() {
 	
@@ -26,22 +24,5 @@ $(function() {
 		$("#preloader").delay(500).fadeOut();
 		$(".preloader").delay(600).fadeOut("slow");
 	});
-
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 1) {
-			$('.dmtop').css({bottom:"25px"});
-		} else {
-  		$('.dmtop').css({bottom:"-100px"});
-		}
-	});
-
-	$('.dmtop').click(function(){
-		$('html, body').animate({scrollTop: '0px'}, 800);
-		return false;
-	});        
-      
-
-
+   
 });
-var router = new Router();
-Backbone.history.start();
