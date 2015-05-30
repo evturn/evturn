a222c4eb75e03852fad19d0bdfc233f5d0b1f058
Evan Turner <evturn@gmail.com>
2015-05-30 15:39:23 -0400

    Revised all project summaries

--- assets/js/collections/portfolio.js
+++ assets/js/collections/portfolio.js
@@ -6,17 +6,7 @@ var Portfolio = Backbone.Collection.extend({
 var musicInsider =
 	{
 		name: 'Music Insider',
-		description: "Music Insider is a Los Angeles based podcast. Using Backbone.js I designed an Admin interface that uses Firebase for User Authentication and real time cloud storage. Clients that are authenticated are given access to admin tools to where the user can add and update posts.",
-		stack: 
-			[
-				backbone, 
-				node,
-				express,
-				photoshop,
-				jQuery,
-				underscore,
-				firebase
-			],
+		description: "Music Insider is a Los Angeles based podcast in which this website hosts along with a blog.",
 		id: 1,
 		url: 'http://musicinsider.co',
 		lead: "assets/img/mi-1.png",
@@ -31,16 +21,7 @@ var musicInsider =
 var marshallz =
 	{
 		name: 'Marshallz Blog',
-		description: ".Using what is called a Markov Chain, several sections of a text file are extracted and then combined to form new, nonsensical blog posts every hour all of which are authored by a fictitious nine year old. In addition to the blog, the server also maintains a Twitter account which is fed posts daily. <a href='http://twitter.com/marshallzBlog' target='_blank'> Follow Marshall <i class='fa fa-twitter'></i></a>",
-		stack:
-			[
-			backbone,
-			node,
-			express,
-			firebase,
-			jQuery,
-			underscore
-			],
+		description: "A blog run by a fictitious nine year old. A new post is authored every hour in addition to sending out <a href='http://twitter.com/marshallzBlog' target='_blank'>sporatic unrelated tweets</a>.",
 		id: 2,
 		url: 'http://marshallz.com',
 		lead: "assets/img/marshallz-1.png",
@@ -53,16 +34,8 @@ var marshallz =
 	};
 var piqueBeta =
 	{
-		name: 'Pique Beta',
+		name: 'Pique',
 		description: "Pique is an app for people who are interested in networking, collaborating, and working on projects. I built the front end of this site which allows potential new users to sign up before the app is released. Views and route parameters are all handled by Backbone. Data from the input fields are saved using Firebase.",
-		stack: 
-			[
-				backbone,
-				firebase,
-				photoshop,
-				jQuery,
-				underscore
-			],
 		id: 3,
 		url: 'http://piqueapp.github.io',
 		lead: "assets/img/pique-1.jpg",
@@ -76,16 +49,7 @@ var piqueBeta =
 var alculator =
 	{
 		name: 'Alculator',
-		description: "Users select from an investory of alcoholic beverages. In addition, users have search access to a database of beer. All alcohol percentages for the user's selected beverages gathered. That data, along with the user's information, is run through the Widmark formula to calculate the individual's blood alcohol level. The results provide a description particular to the user's intoxication level ranging from low traces of alcohol to lethal doses.",
-		stack:
-			[
-				backbone,
-				node,
-				express,
-				photoshop,
-				jQuery,
-				underscore
-			],
+		description: "Alculator is a BAC calculator. User can add items to their tab from a bar with an inventory of standard cocktails, wine by the glass or bottle, and beer. The results include the user's blood alcohol level along with a description of that particular level of intoxication.",
 		id: 4,
 		url: 'http://alculator.co',
 		lead: "assets/img/alculator-3.png",
@@ -98,17 +62,8 @@ var alculator =
 	};
 var ramenBuffet =
 	{
-		name: 'Task Manager | Ramen Buffet',
-		description: "This single page AngularJS app  stores the data from the user input using MongoDB. Users can create an account for the app and in addition login and link their Twitter and Facebook accounts. I have built this app multiple times since the concept provides a great avenue to exploring other technologies. Other iterations of this app used Backbone.js, Marionette.js, and Firebase.", 
-		stack: 
-			[
-				angular,
-				node,
-				express,
-				mongoDB,
-				jQuery,
-				underscore
-			],
+		name: 'Ramen Buffet',
+		description: "Ramen Buffet manages a list of small tasks or todos. These tasks can be of any importance or priority. A clock is provided as timer which can be used to time breaks or time the user may want to set aside to dedicate to a particular task.",
 		id: 5,
 		url: 'http://ramenbuffet.com',
 		lead: 'assets/img/ramen-1.jpg',
@@ -121,18 +76,8 @@ var ramenBuffet =
 var hangman =
 	{
 		name: 'Hangman',
-		description: "Hangman was original built in Sinatra using Ruby. The game state updated in real time and was stored using a PostgreSQL database. Months later I remade the game from with Backbone.js",
-		stack:
-			[
-				backbone,
-				node,
-				express,
-				photoshop,
-				jQuery,
-				underscore
-			],
+		description: "A gory and cartoonish version of the classic Hangman game. An API is used to access a library of words which helps to deliver a more unique game play.",
 		id: 6,
-		url: "http://hangman-node.herokuapp.com",
 		lead: "assets/img/hangman-1.jpg",
 		items: 
 			[
@@ -144,18 +89,23 @@ var hangman =
 var tictactoe =
 	{
 		name: 'Tic Tac Toe',
-		description: "This version of Tic Tac Toe was built using Backbone.js. I sparingly used jQuery for this project giving the entire workload of event handling to my Backbone Views. I designed images for each game piece with Adobe Photoshop. Each image's file path was stored as an attribute to a certain model within the application and then used to render bits and pieces of the game in the browser on initial page load and during gameplay",
-		stack:
-			[
-				backbone,
-				jQuery,
-				underscore
-			],
+		description: "Using photoshop, opponent 'X' displays a random image of two crossing eclairs on each game play, while opponent 'O' is represented with donuts",
 		id: 7,
 		lead: 'assets/img/ttt-1.jpg',
 		items: 
 			[
 				{item: 'assets/img/ttt-2.jpg'}
+			]
+	};
+var drive =
+	{
+		name: 'Drive Publishing',
+		description: "Drive is a music publishing company that manages the catalogues of many new and legendary songwriters and musicians."
+		id: 8,
+		lead: 'assets/img/drive-1.png',
+		items: 
+			[
+				{item: 'assets/img/drive-2.png'}
 			]
 	};
 
