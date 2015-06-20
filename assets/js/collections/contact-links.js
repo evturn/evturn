cdc74ce6b5b788183857db017b6001d93a089b6c
Evan Turner <evturn@gmail.com>
2015-06-19 21:09:55 -0400

    added and created .gitignore for notes

--- assets/js/collections/contact-links.js
+++ assets/js/collections/contact-links.js
@@ -2,37 +2,6 @@ var ContactLinks = Backbone.Collection.extend({
 	model: ContactLink,
 });
 
-var links = [
-	{
-		name: 'email',
-		url: 'mailto:evturn@gmail.com',
-		icon: 'fa fa-envelope-o'
-	},
-	{
-		name: 'github',
-		url: 'http://github.com/evturn',
-		icon: 'fa fa-github-square'
-	},
-	{
-		name: 'linkedin',
-		url: 'http://www.linkedin.com/in/evturn/',
-		icon: 'fa fa-linkedin-square'
-	},
-	{
-		name: 'general assembly',
-		url: 'https://profiles.generalassemb.ly/ev',
-		icon: 'fa fa-certificate'
-	},
-	{
-		name: 'twitter',
-		url: 'http://twitter.com/evturn',
-		icon: 'fa fa-twitter'
-	},
-	{
-		name: 'skype: @evturn',
-		url: 'javaScript:void(0);', // jshint ignore:line
-		icon: 'fa fa-skype',
-	}
-];
+
 
 var contactLinks = new ContactLinks(links);
