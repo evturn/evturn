829b0d5e78164a5e6b7f3631aec6775fc3577476
Evan Turner <evturn@gmail.com>
2015-06-20 04:23:47 -0400

    updated names to templates in contact view

--- assets/js/views/contact.js
+++ assets/js/views/contact.js
@@ -2,15 +2,15 @@ var ev = ev || {};
 
 ev.ContactView = Backbone.View.extend({
 	el: '.contact',
-	containerTemplate: _.template($('#links-container-template').html()),
-	itemTemplate: _.template($('#link-item-template').html()),
+	viewContainer: _.template($('#links-container-template').html()),
+	itemContainer: _.template($('#link-item-template').html()),
 	initialize: function() {
 		this.render();
 	},
 	render: function() {
-		this.$el.html(this.containerTemplate());
+		this.$el.html(this.viewContainer());
 		for (var i = this.collection.length - 1; i >= 0; i--) {
-			$('.contact-links').prepend(this.itemTemplate(this.collection.models[i].toJSON()));
+			$('.contact-links').prepend(this.itemContainer(this.collection.models[i].toJSON()));
 		}
 		return this;
 	},
