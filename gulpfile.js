5de0b19863b0ef94814fcc0c7c496e6283e8e5e2
Evan Turner <evturn@gmail.com>
2015-06-20 16:08:40 -0400

    compressed all vendor javascript files into one

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,24 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
 var js = [
     './assets/js/**/*.js',
     './gulpfile.js'
 ];
 
-gulp.task('default', ['watch', 'sass', 'lint']);
+var vendorJS = [
+  './vendor/js/jquery.js',
+  './vendor/js/underscore.js',
+  './vendor/js/backbone.js',
+  './vendor/js/bootstrap.js',
+  './vendor/js/wow.js'
+];
+
+gulp.task('default', ['watch', 'sass', 'lint', 'compressJS']);
 
 gulp.task('sass', function() {
   return gulp.src('./assets/css/scss/**/*.scss')
@@ -23,6 +33,14 @@ gulp.task('lint', function() {
   return gulp.src('./assets/js/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('compressJS', function() {
+  return gulp.src(vendorJS)
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./vendor'))
+    .on('error', gutil.log);
 });
 
 gulp.task('watch', function() {
