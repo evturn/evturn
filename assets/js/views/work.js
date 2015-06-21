67f28c1a14dd04ebba0e3497c7a5860832b4a870
Evan Turner <evturn@gmail.com>
2015-06-20 21:59:43 -0400

    added preloader to carousel in between projects

--- assets/js/views/work.js
+++ assets/js/views/work.js
@@ -5,12 +5,14 @@ ev.Carousel = Backbone.View.extend({
   viewContainer: _.template($('#carousel-container-template').html()),
   itemContainer: _.template($('#carousel-item-template').html()),
   itemDescription: _.template($('#carousel-panel-template').html()),
+  itemPreloader: _.template($('#carousel-preloader-template').html()),
   initialize: function() {
     this.render();
     this.setChildViews();
   },
   render: function() {
     this.$el.html(this.viewContainer(this.model.toJSON()));
+    this.preloader();
     this.child = new ev.Thumbnails();
     return this;
   },
@@ -22,4 +24,9 @@ ev.Carousel = Backbone.View.extend({
     $('.carousel-panel').html(this.itemDescription(this.model.toJSON()));
     return this;
   },
+  preloader: function() {
+    $('.carousel-image-container').append(this.itemPreloader());
+    $('#carousel-preloader').delay(500).fadeOut();
+    $('.carousel-preloader').delay(600).fadeOut('slow');
+  }
 });
