64a6b7c37972b121ba4958ab843c47ef0ffd8bd9
Evan Turner <evturn@gmail.com>
2015-07-09 00:03:51 -0400

    converted self-invoking functions to arrays

--- assets/js/data/links.js
+++ assets/js/data/links.js
@@ -1,40 +1,38 @@
-var linksObj = (function() { 
-    return [
-    {
-      name: 'email',
-      url: 'mailto:evturn@gmail.com',
-      icon: 'fa fa-envelope',
-      featured: true
-    },
-    {
-      name: 'github',
-      url: 'http://github.com/evturn',
-      icon: 'fa fa-github-square',
-      featured: true
-    },
-    {
-      name: 'linkedin',
-      url: 'http://www.linkedin.com/in/evturn/',
-      icon: 'fa fa-linkedin-square',
-      featured: true
-    },
-    {
-      name: 'general assembly',
-      url: 'https://profiles.generalassemb.ly/ev',
-      icon: 'fa fa-certificate',
-      featured: true
-    },
-    {
-      name: 'twitter',
-      url: 'http://twitter.com/evturn',
-      icon: 'fa fa-twitter',
-      featured: true
-    },
-    {
-      name: 'skype: @evturn',
-      url: 'javaScript:void(0);', // jshint ignore:line
-      icon: 'fa fa-skype',
-      featured: true
-    }
-  ];
-})();
+var linksObj = [
+  {
+    name: 'email',
+    url: 'mailto:evturn@gmail.com',
+    icon: 'fa fa-envelope',
+    featured: true
+  },
+  {
+    name: 'github',
+    url: 'http://github.com/evturn',
+    icon: 'fa fa-github-square',
+    featured: true
+  },
+  {
+    name: 'linkedin',
+    url: 'http://www.linkedin.com/in/evturn/',
+    icon: 'fa fa-linkedin-square',
+    featured: true
+  },
+  {
+    name: 'general assembly',
+    url: 'https://profiles.generalassemb.ly/ev',
+    icon: 'fa fa-certificate',
+    featured: true
+  },
+  {
+    name: 'twitter',
+    url: 'http://twitter.com/evturn',
+    icon: 'fa fa-twitter',
+    featured: true
+  },
+  {
+    name: 'skype: @evturn',
+    url: 'javaScript:void(0);', // jshint ignore:line
+    icon: 'fa fa-skype',
+    featured: true
+  }
+];
