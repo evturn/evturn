7acf357288314fc67b3bd28fa08803f53a7c2020
Evan Turner <evturn@gmail.com>
2015-07-25 05:51:37 -0400

    added variables in animation functions

--- assets/js/evturn/animations.js
+++ assets/js/evturn/animations.js
@@ -1,38 +1,58 @@
 EVTURN.animations = {
+
   init: function() {
     this.preloader();
+
   },
+
   preloader: function() {
+
     $(window).load(function() {
-      $('#preloader').delay(500).fadeOut();
-      $('.preloader').delay(600).fadeOut(600);
+      $container = $('#preloader');
+      $image = $('.preloader');
+
+      $container.delay(500).fadeOut();
+      $image.delay(600).fadeOut(600);
     });
+
   },
+
   statCount: function() {
     $('.stat-count').each(function() {
       $(this).data('count', parseInt($(this).html(), 10));
       $(this).html('0');
+
       EVTURN.animations.count($(this));
     });
+
   },
   count: function($this){
     var current = parseInt($this.html(), 10);
+
     current = current + 50;
     $this.html(++current);
     if (current > $this.data('count')) {
       $this.html($this.data('count'));
-    } else {
+
+    }
+    else {
       setTimeout(function() {
         EVTURN.animations.count($this);
         }, 50);
+
     }
   },
+
   scrollUp: function() {
     $('html, body').animate({scrollTop: 0 }, 500);
+
   },
+
   carouselPreloader: function(template) {
     $('.carousel-image-container').append(template());
     $('#carousel-preloader').delay(500).fadeOut();
     $('.carousel-preloader').delay(600).fadeOut(600);
-  }
+
+  },
+
 };
