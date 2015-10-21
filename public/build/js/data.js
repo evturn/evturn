c111535634a8d56d48ac2736dc6d7868f67c9313
Evan Turner <evturn@gmail.com>
2015-10-21 00:21:19 -0400

    Move video order

--- public/build/js/data.js
+++ public/build/js/data.js
@@ -1,12 +1,12 @@
 
 exports.videos = () => {
   return [
+    'https://www.dropbox.com/s/nijl1tqzivxjlnd/vid-8.m4v?dl=1',
     'https://www.dropbox.com/s/ibiy6fwqjyb5uaw/vid-7.m4v?dl=1',
     'https://www.dropbox.com/s/23upki10se8ve37/vid-15.m4v?dl=1',
     'https://www.dropbox.com/s/8tqgae5yuf7x1n7/vid-6.m4v?dl=1',
     'https://www.dropbox.com/s/0dk58ha0o191qmx/vid-11.m4v?dl=1',
     'https://www.dropbox.com/s/jszss7t0msash80/vid-10.m4v?dl=1',
-    'https://www.dropbox.com/s/nijl1tqzivxjlnd/vid-8.m4v?dl=1',
     'https://www.dropbox.com/s/0c507odqgqwjqv2/vid-3.m4v?dl=1',
     'https://www.dropbox.com/s/dsab5kvchdzvzyp/vid-12.m4v?dl=1',
     'https://www.dropbox.com/s/pinkna2jree0czu/vid-1.m4v?dl=1',
