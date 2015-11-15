75d9b04605c281d6f20e2e7a1610271fd613ec5d
Evan Turner <evturn@gmail.com>
2015-11-15 18:17:04 -0500

    Update project data and images

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -1891,13 +1891,13 @@
 	  technologies: [1, 3, 14, 12, 10, 11, 15, 20]
 	}, {
 	  name: 'Marshallz Blog',
-	  description: "A blog run by a fictitious nine year old. A new post is authored every hour in addition to sending out <a href='http://twitter.com/marshallzBlog' target='_blank'>sporatic unrelated tweets</a>.",
+	  description: "This blog continuously produces new posts authored by bots. These bots get their content by scraping text documents, washing machine manuals, and other sources followed by taking fragments and applying a <a href'https://en.wikipedia.org/wiki/Markov_chain' target'_blank'>Markov Chain</a> to create nonsensical sentences. Before publishing, a randomly selected word from the content is used to search <a href='http://giphy.com' target='_blank'>Giphy</a> and retrieve a related images which is then included in the post. In addition to the website, these same 'authors' post new content to <a href='http://twitter.com/marshallzBlog' target='_blank'>Twitter under their own accounts.</a>",
 	  id: 2,
 	  links: true,
 	  url: 'http://marshallz.com',
 	  repo: 'https://github.com/evturn/marshallz',
 	  thumbnail: 'public/dist/img/apps/marshallz-tn.png',
-	  images: [{ image: 'public/dist/img/apps/marshallz-1.png' }, { image: 'public/dist/img/apps/marshallz-2.png' }, { image: 'public/dist/img/apps/marshallz-6.png' }, { image: 'public/dist/img/apps/marshallz-3.jpg' }, { image: 'public/dist/img/apps/marshallz-5.png' }, { image: 'public/dist/img/apps/marshallz-4.jpg' }],
+	  images: [{ image: 'public/dist/img/apps/marshallz-1.png' }, { image: 'public/dist/img/apps/marshallz-2.png' }, { image: 'public/dist/img/apps/marshallz-3.png' }, { image: 'public/dist/img/apps/marshallz-4.png' }],
 	  featured: true,
 	  technologies: [1, 14, 12, 15, 10, 18, 19, 20]
 	}, {
@@ -1912,6 +1912,16 @@
 	  featured: true,
 	  technologies: [13, 11, 14, 20]
 	}, {
+	  name: 'Brooklyn Friends School',
+	  description: 'I collaborated in the development of the frontend builds that SM&KK Studios had designed for a complete refresh of the Brooklyn Friends School brand.',
+	  id: 9,
+	  links: true,
+	  url: 'http://smkkstudios.com/work/brooklynfriendsschool',
+	  thumbnail: "public/dist/img/apps/bfs-tn.jpg",
+	  images: [{ image: 'public/dist/img/apps/bfs-1.png' }, { image: 'public/dist/img/apps/bfs-2.png' }],
+	  featured: true,
+	  technologies: [21, 13]
+	}, {
 	  name: 'Ramen Buffet',
 	  description: "Ramen Buffet manages multiple lists of tasks or todos. Within these lists, tasks can be sorted by importance, priority, or status.",
 	  id: 6,
@@ -1923,15 +1933,6 @@
 	  featured: true,
 	  technologies: [1, 3, 14, 12, 10, 11, 15, 18, 20]
 	}, {
-	  name: 'Alculator',
-	  description: "Alculator is a BAC calculator. User can add items to their tab from a bar with an inventory of standard cocktails, wine by the glass or bottle, and beer. The results include the user's blood alcohol level along with a description of that particular level of intoxication.",
-	  id: 5,
-	  links: true,
-	  thumbnail: "public/dist/img/apps/alculator-tn.png",
-	  images: [{ image: 'public/dist/img/apps/alculator-3.png' }, { image: 'public/dist/img/apps/alculator-2.png' }, { image: 'public/dist/img/apps/alculator-1.png' }, { image: 'public/dist/img/apps/alculator-4.png' }],
-	  featured: true,
-	  technologies: [1, 12, 3, 11]
-	}, {
 	  name: 'Pique',
 	  description: "Pique is an app for people who are interested in networking, collaborating, and working on projects.",
 	  id: 3,
@@ -1942,31 +1943,14 @@
 	  featured: true,
 	  technologies: [16, 17, 11]
 	}, {
-	  name: 'Hangman',
-	  description: "A gory and cartoonish version of the classic Hangman game. An API is used to access a library of words which helps to deliver a more unique game play.",
-	  id: 7,
-	  thumbnail: "public/dist/img/apps/hangman-tn.png",
-	  images: [{ image: 'public/dist/img/apps/hangman-1.jpg' }, { image: 'public/dist/img/apps/hangman-2.jpg' }],
-	  featured: false,
-	  technologies: [1, 12, 3, 11]
-	}, {
-	  name: 'Tic Tac Toe',
-	  description: "Using photoshop, opponent 'X' displays a random image of two crossing eclairs on each game play, while opponent 'O' is represented with donuts",
-	  id: 8,
-	  thumbnail: "public/dist/img/apps/ttt-tn.png",
-	  images: [{ lead: 'public/dist/img/apps/ttt-1.jpg' }, { image: 'public/dist/img/apps/ttt-2.jpg' }],
-	  featured: false,
-	  technologies: [3, 11]
-	}, {
-	  name: 'Brooklyn Friends School',
-	  description: 'I collaborated in the development of the frontend builds that SM&KK Studios had designed for a complete refresh of the Brooklyn Friends School brand.',
-	  id: 9,
+	  name: 'Alculator',
+	  description: "User add items to their tab from a bar with an inventory of standard cocktails, wine by the glass or bottle, and beer. The results include the user's blood alcohol level along with a description of that particular level of intoxication.",
+	  id: 5,
 	  links: true,
-	  url: 'http://smkkstudios.com/work/brooklynfriendsschool',
-	  thumbnail: "public/dist/img/apps/bfs-tn.jpg",
-	  images: [{ image: 'public/dist/img/apps/bfs-1.png' }, { image: 'public/dist/img/apps/bfs-2.png' }],
+	  thumbnail: "public/dist/img/apps/alculator-tn.png",
+	  images: [{ image: 'public/dist/img/apps/alculator-2.png' }, { image: 'public/dist/img/apps/alculator-3.png' }, { image: 'public/dist/img/apps/alculator-1.png' }, { image: 'public/dist/img/apps/alculator-4.png' }],
 	  featured: true,
-	  technologies: [21, 13]
+	  technologies: [1, 12, 3, 11]
 	}, {
 	  name: 'WhereTO',
 	  description: 'Search venues around you and bookmark spots. Create custom lists of places you want to remember and can reference when you want to try something new.',
