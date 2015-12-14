15ce485acf013dc7cd140fd26872e8accedc18fc
Evan Turner <evturn@gmail.com>
2015-12-13 19:29:38 -0500

    Create footer

---
+++ src/dist/0.6ed8caae09b5995ab23e.hot-update.js
@@ -0,0 +1,38 @@
+webpackHotUpdate(0,{
+
+/***/ 364:
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(module) {/* REACT HOT LOADER */ if (true) { (function () { var ReactHotAPI = __webpack_require__(110), RootInstanceProvider = __webpack_require__(118), ReactMount = __webpack_require__(120), React = __webpack_require__(172); module.makeHot = module.hot.data ? module.hot.data.makeHot : ReactHotAPI(function () { return RootInstanceProvider.getRootInstances(ReactMount); }, React); })(); } try { (function () {
+	
+	'use strict';
+	
+	exports.__esModule = true;
+	exports.Footer = undefined;
+	
+	var _react = __webpack_require__(172);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var Footer = exports.Footer = _react2.default.createClass({
+	  displayName: 'Footer',
+	  render: function render() {
+	    return _react2.default.createElement(
+	      'footer',
+	      { className: 'site-footer' },
+	      '© 2015 evturn.com built with ',
+	      _react2.default.createElement('span', { className: 'devicon-react-plain-wordmark' }),
+	      'React'
+	    );
+	  }
+	});
+	
+	/* REACT HOT LOADER */ }).call(this); } finally { if (true) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = __webpack_require__(327); if (makeExportsHot(module, __webpack_require__(172))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot not apply hot update to " + "Footer.jsx" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(37)(module)))
+
+/***/ }
+
+})
+//# sourceMappingURL=0.6ed8caae09b5995ab23e.hot-update.js.map
