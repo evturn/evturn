ec53741f17a961df42ba407d7fa32fe99ceb1325
Evan Turner <evturn@gmail.com>
2015-12-22 17:34:54 +0800

    Update to use optimize occurence

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     publicPath: 'dist/'
   },
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
