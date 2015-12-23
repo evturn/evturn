8bf6d9ab3b8817550c0070e4486e49a9d378ee2f
Evan Turner <evturn@gmail.com>
2015-12-23 15:20:48 +0800

    Update Bio component styles

--- src/components/about/bio.jsx
+++ src/components/about/bio.jsx
@@ -1,22 +1,20 @@
 'use strict';
 import React from 'react';
 import CSSModules from 'react-css-modules';
-import css from './bio.pre';
-
-const p1 = 'As the web continues to evolve, exploring solutions and strategies for building rich applications is not only essential but provides an exciting opportunity for design innovation.';
+import styles from './bio.pre';
 
 const Bio = React.createClass({
   getDefaultProps() {
     return {
       title: 'Web Developer',
-      bio: p1
+      bio: 'As the web continues to evolve, exploring solutions and strategies for building rich applications is not only essential but provides an exciting opportunity for design innovation.';
     };
   },
   render() {
     return (
-      <div className={css.root}>
-        <div className={css.header}>{this.props.title}</div>
-        <div className={css.paragraph}>{this.props.bio}</div>
+      <div styleName='root'>
+        <div styleName='header'>{this.props.title}</div>
+        <div styleName='paragraph'>{this.props.bio}</div>
       </div>
     );
   }
