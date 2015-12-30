820998875f81481734a025f237e6dad9bbaba0d5
Evan Turner <evturn@gmail.com>
2015-12-31 00:28:51 +0800

    Reorder loading of ReactDOM

--- src/run.jsx
+++ src/run.jsx
@@ -1,18 +1,18 @@
 import ReactDOM from 'react-dom';
+import {default as Spinner} from 'components/spinner';
+
+ReactDOM.render(<Spinner />, document.getElementById('loading'));
+
+
 import {Router, Route, IndexRoute} from 'react-router';
 import {createHashHistory} from 'history';
-import {default as Spinner} from 'components/spinner';
 import {default as App} from 'containers/app';
 import {default as Home} from 'containers/home';
 import {default as About} from 'containers/about';
 import {default as Contact} from 'containers/contact';
 import {default as Work} from 'containers/work';
-import 'styles/style.less';
-
-ReactDOM.render(<Spinner/>, document.getElementById('loading'));
 
 const history = createHashHistory({queryKey: false});
-
 ReactDOM.render((
   <Router history={history}>
     <Route path="/" component={App}>
