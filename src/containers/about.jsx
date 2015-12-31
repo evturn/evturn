05219587ff87f3d975dab36e153dc6b1da16aed0
Evan Turner <evturn@gmail.com>
2015-12-31 09:46:55 +0800

    Remove helper function from About component

--- src/containers/about.jsx
+++ src/containers/about.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {CounterItems as AboutStat} from 'components/icon-count';
 import {TechItems as AboutTech} from 'components/icon-tech';
 import {default as __stats} from 'sources/stats';
-import {setFeaturedTech} from 'helpers';
+import {default as __tech} from 'sources/tech';
 
 const AboutHeader = React.createClass({
   render() {
@@ -12,25 +12,32 @@ const AboutHeader = React.createClass({
 });
 
 export default React.createClass({
-  getDefaultProps() {
-    return {
-      bio: 'As the web continues to evolve, exploring solutions and strategies for building rich applications is not only essential but provides an exciting opportunity for design innovation.',
-      stats: __stats,
-      tech: setFeaturedTech()
-    };
+  getTechItems() {
+    return __tech.filter((obj) => {
+      return obj.featured
+    }).map((obj) => {
+      return obj;
+    });
   },
   getInitialState() {
     return {
-      ...this.props
+      bio: 'As the web continues to evolve, exploring solutions and strategies for building rich applications is not only essential but provides an exciting opportunity for design innovation.',
+      stats: __stats,
+      tech: this.getTechItems(),
+      bioTitle: 'Web Developer',
+      techTitle: 'Notable Tools',
+      statsTitle: 'Statistics'
     };
   },
-  componentDidMount() {
-    return this.setState({
-      ...this.props
-    });
-  },
   render() {
-    const {bio, tech, stats} = this.state;
+    const {
+      bio,
+      tech,
+      stats,
+      bioTitle,
+      techTitle,
+      statsTitle
+    } = this.state;
 
     return (
       <div className='about'>
@@ -38,17 +45,17 @@ export default React.createClass({
           <img className='about-image' src='src/images/site/skel.gif' />
         </div>
         <div className='bio'>
-        <AboutHeader title='Web Developer' />
+        <AboutHeader title={bioTitle} />
           <div className='paragraph'>{bio}</div>
         </div>
 
         <div className='about-info'>
           <div className='about-tech'>
-            <AboutHeader title='Notable Tools' />
+            <AboutHeader title={techTitle} />
             <AboutTech items={tech} width={'item-20'} />
           </div>
           <div className='about-stats'>
-            <AboutHeader title='Statistics' />
+            <AboutHeader title={statsTitle} />
             <AboutStat items={stats} width={'item-50'} />
           </div>
         </div>
