f6914bb02638934a1b83d7ea6b655ee95b6b9728
Evan Turner <evturn@gmail.com>
2016-01-03 01:07:37 +0800

    Remove ReactDOM and add createHashHistory

--- src/run.jsx
+++ src/run.jsx
@@ -1,16 +1,13 @@
-import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute} from 'react-router';
+import {createHashHistory} from 'history';
 import {default as App} from 'containers/app';
 import {default as Home} from 'containers/home';
 import {default as About} from 'containers/about';
 import {default as Contact} from 'containers/contact';
 import {default as Work} from'containers/work';
 
-const history = browserHistory;
-const el = document.getElementById('site-container');
-
-ReactDOM.render((
-  <Router history={history}>
+const router = (
+  <Router history={createHashHistory()}>
     <Route component={App} path='/' >
       <IndexRoute component={Home} name='home' />
       <Route component={About} path='about' name='about' />
@@ -18,4 +15,6 @@ ReactDOM.render((
       <Route component={Work} path='work(/:id)' name='work' />
     </Route>
   </Router>
-), el);
+);
+
+React.render(router, document.getElementById('site-container'));
