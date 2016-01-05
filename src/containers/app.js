d7dbca70bc31a87ccaa6b5ec12777def0db596ae
Evan Turner <evturn@gmail.com>
2016-01-05 12:56:55 -0500

    Move layout outside return statement in App component

--- src/containers/app.js
+++ src/containers/app.js
@@ -11,16 +11,20 @@ export const App = React.createClass({
       this.setState({ page: route });
     }
   },
+  getInitialState() {
+    return { page: this.props.routes[1].name };
+  },
   componentDidMount() {
     this.setState({ page: this.props.routes[1].name });
   },
   render() {
-    return (
+    const site = (
       <div>
-        <Header {...this.props} {...this.state} />
+        <Header page={this.state.page} />
         {this.props.children}
-        <Footer {...this.props} {...this.state} />
+        <Footer page={this.state.page} />
       </div>
     );
+    return site;
   }
 });
