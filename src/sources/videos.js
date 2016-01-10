bdf516182ad04af9135bf339cc46ea74d5248613
Evan Turner <evturn@gmail.com>
2016-01-10 02:44:22 -0500

    Update playlist order

--- src/sources/videos.js
+++ src/sources/videos.js
@@ -2,12 +2,12 @@
 export default [
   'https://www.dropbox.com/s/ibiy6fwqjyb5uaw/vid-7.m4v?dl=1',
   'https://www.dropbox.com/s/nijl1tqzivxjlnd/vid-8.m4v?dl=1',
+  'https://www.dropbox.com/s/0c507odqgqwjqv2/vid-3.m4v?dl=1',
   'https://www.dropbox.com/s/23upki10se8ve37/vid-15.m4v?dl=1',
   'https://www.dropbox.com/s/pinkna2jree0czu/vid-1.m4v?dl=1',
   'https://www.dropbox.com/s/8tqgae5yuf7x1n7/vid-6.m4v?dl=1',
   'https://www.dropbox.com/s/0dk58ha0o191qmx/vid-11.m4v?dl=1',
   'https://www.dropbox.com/s/jszss7t0msash80/vid-10.m4v?dl=1',
-  'https://www.dropbox.com/s/0c507odqgqwjqv2/vid-3.m4v?dl=1',
   'https://www.dropbox.com/s/dsab5kvchdzvzyp/vid-12.m4v?dl=1',
   'https://www.dropbox.com/s/p56i6t3gxwbypbs/vid-16.m4v?dl=1',
   'https://www.dropbox.com/s/a7vmoy155re7drv/vid-18.m4v?dl=1',
