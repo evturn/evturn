8ff3d9cb5f134651da29f3c87c136be6991666de
Evan Turner <evturn@gmail.com>
2016-01-23 18:30:45 -0500

    Rename header component file

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from 'components/Footer';
-import Header from 'components/header';
+import Header from 'components/Header';
 import 'styles/base.less';
 import 'sources/google-analytics';
 
