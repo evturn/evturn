bb4c7858a310cf3f21e740f6658f971e97004e72
Evan Turner <evturn@gmail.com>
2016-02-10 16:45:01 -0500

    Update function declarations for data loading

--- src/server/data/index.js
+++ src/server/data/index.js
@@ -3,30 +3,24 @@ import TECH from 'db/tech';
 
 const PATH = 'src/client/assets/';
 
-const setProjectsNav = () => {
-  return PROJECTS.map(project => {
+function filterFeaturedTech() {
+  return TECH.filter(obj => obj.featured).map(obj => obj);
+}
+
+function filterProjectTech(ids) {
+  return TECH.filter(o => ids.indexOf(o.id) !== -1 ? true : false);
+}
+
+function setProjectsNav() {
+  return PROJECTS.map(obj => {
     return {
-      id: project.id,
-      image: `${PATH}${project.thumbnail}`
+      id: obj.id,
+      image: `${PATH}${obj.thumbnail}`
     };
   });
-};
+}
 
-const filterFeaturedTech = () => {
-  return TECH.filter(obj => {
-    return obj.featured;
-  }).map(obj => {
-    return obj;
-  });
-};
-
-const filterProjectTech = (ids) => {
-  return TECH.filter(o => {
-    return ids.indexOf(o.id) !== -1 ? true : false;
-  });
-};
-
-const setProjects = () => {
+function setProjects() {
   return PROJECTS.map(project => {
     const { techIds, images, ...props } = project;
 
@@ -35,7 +29,7 @@ const setProjects = () => {
       slides: images.map(image => `${PATH}${image}`)
     });
   });
-};
+}
 
 export const projectsNav = setProjectsNav();
 export const projects = setProjects();
