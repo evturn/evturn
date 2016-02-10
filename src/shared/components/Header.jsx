ab3919e1c2c3a4357e93cd642c6540d608706cc7
Evan Turner <evturn@gmail.com>
2016-02-10 02:45:39 -0500

    Use all reuseable components for Header component

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
-import SiteNavigation from 'components/SiteNavigation';
-import SiteNavbar from 'components/SiteNavbar';
+import { IndexLink, Link } from 'react-router';
+import FontIcon from 'components/FontIcon';
+import classNames from 'classnames/bind';
+import styles from 'css/components/nav.less';
+import {
+  UnorderedList, AbsoluteContainer,
+  Image, ListItem, SectionContainer
+} from 'components/reuseables';
+
+const cx = classNames.bind(styles);
 
 export default ({ open, page, pages, toggle }) => {
   return (
     <header>
-      <SiteNavbar
-        page={page}
-        toggle={toggle}
-      />
-      <SiteNavigation
-        open={open}
-        page={page}
-        pages={pages}
-        toggle={toggle}
-      />
+      <AbsoluteContainer classname={cx('logo', page)}>
+        <Image src="src/client/assets/images/site/ev-av.png" />
+      </AbsoluteContainer>
+      <AbsoluteContainer classname={cx('burger', page)} onClick={() => toggle()}>
+        <FontIcon type={'fa'} name={'fa-bars'} />
+      </AbsoluteContainer>
+
+      <nav className={cx('menu', {'in': open, 'out': !open})}>
+        <SectionContainer>
+          <SectionContainer classname={cx('close')} onClick={() => toggle()}>
+            <FontIcon type={'fa'} name={'fa-times'} />
+          </SectionContainer>
+        </SectionContainer>
+        <UnorderedList>{pages.map((page, i) => {
+          const { route, name, id } = page;
+          const link = id === 1 ?
+            <IndexLink to={route} children={name} /> :
+            <Link to={route} children={name} />;
+
+          return (
+            <ListItem key={i} className={cx('item')} onClick={() => toggle()} children={link} />
+          );
+        })}</UnorderedList>
+      </nav>
     </header>
   );
 }
--- src/shared/components/reuseables/index.js
+++ src/shared/components/reuseables/index.js
@@ -5,65 +5,73 @@ import styles from 'css/reuseables.less';
 const cx = classNames.bind(styles);
 
 export const PageContainer = props => {
-  const { children, classname } = props;
+  const { children, classname, ...rest } = props;
 
   return (
-    <div className={cx('page', classname)}>{children}</div>
+    <div className={cx('page', classname)} {...rest}>{children}</div>
   );
 }
 
 export const SectionContainer = props => {
-  const { children, classname } = props;
+  const { children, classname, ...rest } = props;
 
   return (
-    <div className={cx('section', classname)}>{children}</div>
+    <div className={cx('relative', classname)} {...rest}>{children}</div>
+  );
+}
+
+export const AbsoluteContainer = props => {
+  const { children, classname, ...rest } = props;
+
+  return (
+    <div className={cx('absolute', classname)} {...rest}>{children}</div>
   );
 }
 
 export const TextHeader = props => {
-  const { text, classname } = props;
+  const { text, classname, ...rest } = props;
 
   return (
-    <div className={cx('header', classname)}>{text}</div>
+    <div className={cx('header', classname)} {...rest}>{text}</div>
   );
 }
 
 export const TextTitle = props => {
-  const { text, classname } = props;
+  const { text, classname, ...rest } = props;
 
   return (
-    <div className={cx('title', classname)}>{text}</div>
+    <div className={cx('title', classname)} {...rest}>{text}</div>
   );
 }
 
 export const TextParagraph = props => {
-  const { text, classname } = props;
+  const { text, classname, ...rest } = props;
 
   return (
-    <div className={cx('paragraph', classname)}>{text}</div>
+    <div className={cx('paragraph', classname)} {...rest}>{text}</div>
   );
 }
 
 export const Image = props => {
-  const { src, classname } = props;
+  const { src, classname, ...rest } = props;
 
   return (
-    <img className={cx('img-scale', classname)} src={src} />
+    <img className={cx('img-scale', classname)} src={src} {...rest} />
   );
 }
 
 export const UnorderedList = props => {
-  const { classname, children } = props;
+  const { classname, children, ...rest } = props;
 
   return (
-    <ul className={cx('unordered-list', classname)}>{children}</ul>
+    <ul className={cx('unordered-list', classname)} {...rest}>{children}</ul>
   );
 }
 
 export const ListItem = props => {
-  const { width, classname, children } = props;
+  const { width, classname, children, ...rest } = props;
 
   return (
-    <li style={ {width} } className={cx('list-item', classname)}>{children}</li>
+    <li style={ {width} } className={cx('list-item', classname)} {...rest}>{children}</li>
   );
 }
