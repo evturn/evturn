88d79939cc20c0a821b71a53a9a9208c795dd166
Evan Turner <evturn@gmail.com>
2016-02-28 22:34:25 -0500

    Create iOS page

--- src/server/db/work-ios.js
+++ src/server/db/work-ios.js
@@ -1,9 +1,11 @@
 export default [
   {
-    name: 'Tormenta',
-    id: 1
+    name: 'Troposphere',
+    id: 1,
+    image: 'troposphere.png'
   },{
     name: 'Mixtape',
-    id: 2
+    id: 2,
+    image: 'mixtape.png'
   }
 ];
--- src/shared/components/WorkIOS.js
+++ src/shared/components/WorkIOS.js
@@ -1,12 +1,37 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import classNames from 'classnames/bind';
+import styles from '../../client/less/components/work-ios.less';
+import { AppImage, UnorderedList, ListItem } from '../components/reuseables';
+
+const cx = classNames.bind(styles);
 
 export default class WorkIOS extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return (
-      <div>iOS</div>
-    );
-  }
+      return (
+        <UnorderedList className={cx('items')}>{this.props.iOS.map((item, i ) => {
+          return (
+            <ListItem key={i} className={cx('item')}>
+              <div className={cx('name')}>{item.name}</div>
+              <AppImage src={item.image} />
+            </ListItem>
+          );
+        })}</UnorderedList>
+      );
+    }
 }
+
+WorkIOS.propTypes = {
+  OSS: PropTypes.array
+};
+
+function mapStateToProps(state) {
+  return {
+    iOS: state.work.iOS
+  };
+}
+
+export default connect(mapStateToProps)(WorkIOS);
