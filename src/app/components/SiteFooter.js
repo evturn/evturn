f782f1c7977361913753f329fb45e87a670f602f
Evan Turner <evturn@gmail.com>
2016-04-11 03:45:24 -0400

    Update footer text

--- src/app/components/SiteFooter.js
+++ src/app/components/SiteFooter.js
@@ -7,7 +7,7 @@ const cx = classNames.bind(styles);
 export default ({ page }) => {
   return (
     <footer className={cx('footer', page)}>
-      <div className={cx('text')}>Evan Turner | evturn.com  <span className="fa fa-copyright" /> 2016 built with <span className={cx('icon', 'devicon-react-plain-wordmark')} /></div>
+      <div className={cx('text')}>Evan Turner | evturn.com  <span className="fa fa-copyright" /> 2016</div>
     </footer>
   );
 }
