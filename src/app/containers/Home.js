f93951d3a0972010cd1724dd2790d320e082aafe
Evan Turner <evturn@gmail.com>
2016-04-13 16:11:59 -0400

    Split home page desktop and mobile into sections

--- src/app/containers/Home.js
+++ src/app/containers/Home.js
@@ -12,34 +12,43 @@ const cx = classNames.bind(css);
 class Home extends Component {
   render() {
     const { mobileNav } = this.props;
+    const background = {
+      backgroundImage: `url(${require('site-images/banana-plants.png')})`
+    };
+    const desktop = (
+      <div className={cx('lg')} onClick={next}>
+        <Spinner />
+        <VideoPlayer />
+        <div className={cx('title')}>
+          <img className="img" src="dist/img/title-white.svg" />
+          <div className={cx('subtitle')} />
+        </div>
+      </div>
+    );
+
+    const mobile = (
+      <div className={cx('sm')}>
+        <div className={cx('title')}>
+          <img className="img" src="dist/img/title-white.svg" />
+          <div className={cx('subtitle')} />
+        </div>
+
+        <img className={cx('av')} src={require('site-images/ev-av.png')} />
+
+        <ul className={cx('top')}>{mobileNav.map(page =>
+          <li key={page.name} className={cx('item')}>
+            <Link to={page.route}>{page.name}</Link>
+          </li>
+        )}</ul>
+
+        <div className={cx('overlay')} />
+      </div>
+    );
 
     return (
-      <div style={{ backgroundImage: `url(${require('site-images/banana-plants.png')})` }} className={cx('root')}>
-        <div className={cx('lg')} onClick={next}>
-          <Spinner />
-          <VideoPlayer />
-          <div className={cx('title')}>
-            <img className="img" src="dist/img/title-white.svg" />
-            <div className={cx('subtitle')} />
-          </div>
-        </div>
-
-        <div className={cx('sm')}>
-          <div className={cx('title')}>
-            <img className="img" src="dist/img/title-white.svg" />
-            <div className={cx('subtitle')} />
-          </div>
-
-          <img className={cx('av')} src={require('site-images/ev-av.png')} />
-
-          <ul className={cx('top')}>{mobileNav.map(page =>
-            <li key={page.name} className={cx('item')}>
-              <Link to={page.route}>{page.name}</Link>
-            </li>
-          )}</ul>
-
-          <div className={cx('overlay')} />
-        </div>
+      <div style={background} className={cx('root')}>
+        {desktop}
+        {mobile}
       </div>
     );
   }
