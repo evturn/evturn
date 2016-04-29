60c4aec45ab0256cded5eb3adf1f00cafbd6c51b
Evan Turner <evturn@gmail.com>
2016-04-29 04:11:17 -0400

    Add Rx to video player actions

--- src/app/actions/video.js
+++ src/app/actions/video.js
@@ -1,49 +1,45 @@
-const actions = {
-  mountVideoPlayer:   _ => ({ type: 'MOUNT_VIDEO_PLAYER' }),
-  fadeLoadingScreen:  _ => ({ type: 'FADE_LOADING_SCREEN' }),
-  killLoadingScreen:  _ => ({ type: 'KILL_LOADING_SCREEN' }),
-  loadNextVideo:      _ => ({ type: 'LOAD_NEXT_VIDEO' }),
-  unmountVideoPlayer: _ => ({ type: 'UNMOUNT_VIDEO_PLAYER' })
-};
+import { Observable } from 'rx'
+
+const MOUNT_VIDEO_PLAYER   = _ => ({ type: 'MOUNT_VIDEO_PLAYER' })
+const KILL_LOADING_SCREEN  = _ => ({ type: 'KILL_LOADING_SCREEN' })
+const FADE_LOADING_SCREEN  = _ => ({ type: 'FADE_LOADING_SCREEN' })
+const LOAD_NEXT_VIDEO      = _ => ({ type: 'LOAD_NEXT_VIDEO' })
+const UNMOUNT_VIDEO_PLAYER = _ => ({ type: 'UNMOUNT_VIDEO_PLAYER' })
 
 const killLoadingScreen = () => dispatch => {
-  dispatch(actions.fadeLoadingScreen({ ready: true }));
-  setTimeout(() => dispatch(actions.killLoadingScreen({ done: true })), 1000);
-};
-
-const initializeVideoPlayer = player => dispatch => {
-  const timeout = setTimeout(() => dispatch(killLoadingScreen()), 3000);
-  let initialized = false;
-
-  dispatch(actions.mountVideoPlayer());
-
-  player.addEventListener('playing',
-    () => {
-      if (!initialized) {
-        clearTimeout(timeout);
-        dispatch(killLoadingScreen());
-        initialized = true;
-      }
-
-      player.playbackRate = 0.6;
-    }
-  );
-
-  player.addEventListener('ended',
-    () => dispatch(actions.loadNextVideo())
-  );
+  Observable.timer(1000)
+    .map(dispatch(FADE_LOADING_SCREEN({ ready: true })))
+    .subscribe(_ => dispatch(KILL_LOADING_SCREEN({ done: true })))
 }
 
 export const mountVideoPlayer = player => dispatch => {
   if (window.innerWidth < 600) {
-    return dispatch(killLoadingScreen());
+    return dispatch(killLoadingScreen())
   }
 
-  dispatch(initializeVideoPlayer(player));
-};
+  dispatch(MOUNT_VIDEO_PLAYER())
+
+  const timer$ = Observable.timer(3000)
+  const timeout$ = timer$.subscribe(_ => dispatch(killLoadingScreen()))
+  let initialized = false;
+
+  Observable.fromEvent(player, 'playing')
+    .subscribe(_ => {
+      if (!initialized) {
+        timeout$.dispose()
+        dispatch(killLoadingScreen())
+        initialized = true
+      }
+
+      player.playbackRate = 0.6;
+    })
+
+  Observable.fromEvent(player, 'ended')
+    .subscribe(_ => dispatch(LOAD_NEXT_VIDEO()))
+}
 
 export const unmountVideoPlayer = () => dispatch => {
-  dispatch(actions.unmountVideoPlayer());
-};
+  dispatch(UNMOUNT_VIDEO_PLAYER())
+}
 
-export const loadNextVideo = () => dispatch => dispatch(actions.loadNextVideo());
+export const loadNextVideo = () => dispatch => dispatch(LOAD_NEXT_VIDEO())
