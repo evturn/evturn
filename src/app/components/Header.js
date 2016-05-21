3e956c987bef9192a865cdd576821df6a7bf3d4d
Evan Turner <evturn@gmail.com>
2016-05-21 03:14:27 -0400

    Move Header elements into variables

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -11,32 +11,37 @@ class Header extends Component {
   render() {
     const { page, nav, open } = this.props
 
+    const navbar = page !== 'home' ?
+      <div className={cx('nav')}>
+        <Link to='/'><img src="dist/img/title-white.svg" /></Link>
+      </div> : null
+
+    const burger = (
+      <div className={cx('burger', { home: page === 'home' })} onClick={toggleMenu}>
+        <span className={'fa fa-bars'} />
+      </div>
+    )
+
+    const menu = (
+      <nav className={cx('menu', { 'in': open, 'out': !open })}>
+        <div className={cx('header')}>
+          <div className={cx('close')} onClick={toggleMenu}>
+            <img className="img" src={require('site-images/close-light.png')} />
+          </div>
+        </div>
+        <ul className={cx('links')}>{nav.map((x, i) =>
+          <li key={i} className={cx('item')} onClick={toggleMenu}>
+            <Link to={x.route}>{x.name}</Link>
+          </li>
+        )}</ul>
+      </nav>
+    )
+
     return (
       <header>
-        {page !== 'home' ? (
-          <div className={cx('nav')}>
-            <Link to='/'><img src="dist/img/title-white.svg" /></Link>
-          </div>
-        ) : null}
-
-        <div className={cx('burger', { home: page === 'home' })} onClick={toggleMenu}>
-          <span className={'fa fa-bars'} />
-        </div>
-
-        <nav className={cx('menu', {'in': open, 'out': !open})}>
-          <div className={cx('header')}>
-            <div className={cx('close')} onClick={toggleMenu}>
-              <img className="img" src={require('site-images/close-light.png')} />
-            </div>
-          </div>
-
-          <ul className={cx('links')}>{nav.map((x, i) =>
-            <li key={i} className={cx('item')} onClick={toggleMenu}>
-              <Link to={x.route}>{x.name}</Link>
-            </li>
-          )}</ul>
-
-        </nav>
+        {navbar}
+        {burger}
+        {menu}
       </header>
     )
   }
