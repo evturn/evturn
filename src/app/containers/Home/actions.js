d2a89b2125f8f0277bd4e56b773cefe6cc7a61c4
Evan Turner <evturn@gmail.com>
2016-05-24 12:30:12 -0400

    Update video player actions

--- src/app/containers/Home/actions.js
+++ src/app/containers/Home/actions.js
@@ -10,18 +10,39 @@ import {
   ADJUST_PLAYBACK_RATE
 } from './constants'
 
+const beginFade = _ => (
+  Rx.Observable.of({ ready: true })
+    .map(payload => ({ type: FADE_LOADING_SCREEN, payload }))
+)
+const completeFadeWithTime = duration => (
+  Rx.Observable.timer(duration)
+    .map(_ => ({ done: true }))
+    .map(payload => ({ type: KILL_LOADING_SCREEN, payload  }))
+)
+
+const initializePlayer = _ => (
+  Rx.Observable.of({ initialized: true })
+    .map(payload => ({ type: INITIALIZE_PLAYER, payload }))
+)
+
+const timeoutWithAbort = duration => actions => (
+  Rx.Observable.timer(duration)
+    .flatMap(beginFade)
+    .takeUntil(actions.ofType(INITIALIZE_PLAYER))
+)
+
 export const mountPlayer = player => (
   (actions, store) => {
+    const bailOnInitialize$ = Rx.Observable.merge(
+      completeFadeWithTime(800),
+      beginFade()
+    )
+
     if (window.innerWidth <= 1024) {
-      return Rx.Observable.of({ done: true, ready: true })
-        .map(payload => ({ type: KILL_LOADING_SCREEN, payload }))
+      return bailOnInitialize$
     }
 
-    store.dispatch(actions => Rx.Observable.timer(3000)
-      .map(_ => ({ done: true }))
-      .map(payload => ({ type: KILL_LOADING_SCREEN, payload }))
-      .takeUntil(actions.ofType(INITIALIZE_PLAYER))
-    )
+    store.dispatch(timeoutWithAbort(3000))
 
     const playlist = store.getState().video.playlist
 
@@ -30,29 +51,25 @@ export const mountPlayer = player => (
       .map(x => ({ src: x, id: 0 }))
       .map(payload => ({ type: MOUNT_VIDEO_PLAYER, payload }))
 
-    const playing$ = Rx.Observable.fromEvent(player, 'playing')
-      .map(x => {
-        if (!store.getState().video.initialized) {
-          store.dispatch(_ => Rx.Observable.timer(800)
-            .map(_ => ({ done: true }))
-            .map(payload => ({ type: KILL_LOADING_SCREEN, payload  }))
-          )
-          store.dispatch(_ => Rx.Observable.of({ initialized: true, ready: true })
-            .map(payload => ({ type: INITIALIZE_PLAYER, payload }))
-          )
-        }
-
-        player.playbackRate = 0.6
-      })
-      .map(_ => ({ type: ADJUST_PLAYBACK_RATE }))
-
     const ended$ = Rx.Observable.fromEvent(player, 'ended')
       .map(x => store.getState().video.id)
       .map(id => id === playlist.length - 1 ? 0 : id + 1)
       .map(x => ({ src: playlist[x], id: x }))
       .map(payload => ({ type: LOAD_NEXT_VIDEO, payload }))
 
-    return Rx.Observable.merge(start$, playing$, ended$)
+    const playing$ = Rx.Observable.fromEvent(player, 'playing')
+      .map(x => {
+        if (!store.getState().video.initialized) {
+          store.dispatch(_ => completeFadeWithTime(800))
+          store.dispatch(initializePlayer)
+          store.dispatch(beginFade)
+        }
+
+        player.playbackRate = 0.6
+      })
+      .map(_ => ({ type: ADJUST_PLAYBACK_RATE }))
+
+    return Rx.Observable.merge(start$, ended$, playing$)
   }
 )
 
