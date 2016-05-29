04d7b7f03ed16f9c0bd570e42775d13e6f0cd2ee
Evan Turner <evturn@gmail.com>
2016-05-29 11:35:41 -0400

    Add Project and screenshots

--- src/internals/data/web.js
+++ src/internals/data/web.js
@@ -26,7 +26,7 @@ export default [
     tech: ['node', 'gulp', 'sass']
   },{
     name: 'Marshallz Blog',
-    description: `Built using React, Redux, and RxJS, this program continuously produces new content in the form of blog posts and tweets authored by bots. These bots are assigned their own API keys and destinations for scraping text various documents, such as news articles and washing machine manuals. Fragments of these sentences are collected and run through a process creating a Markov Chain resulting nonsensical sentences. If the bot is publishing to the blog, content from these sentences is used to search Giphy and retrieve an image which is then included in the post.`,
+    description: `Built using React, Redux, and RxJS, this program continuously posts new content to the website in addition to posting on Twitter. Content for these posts are gathered anywhere from RSS feeds to online washing machine manuals.`,
     id: 2,
     slug: 'marshallz',
     links: [
@@ -48,14 +48,13 @@ export default [
     images: [
       'marshallz-1.png',
       'marshallz-2.png',
-      'marshallz-3.png',
-      'marshallz-4.png'
+      'marshallz-3.png'
     ],
     featured: true,
     tech: ['node', 'react', 'redux', 'mongodb', 'rxjs', 'webpack', 'less']
   },{
     name: 'Nutty Goodness',
-    description: 'Nutty Goodness is a producer of healthy snacks made from all-natural fruit & nuts. I developed a brand new website for the company which was led by New York City based product design and development shop Born & Raised.',
+    description: 'Led by New York City based product design and development shop Born & Raised for health snack producer, Nutty Goodness.',
     id: 3,
     slug: 'nutty-goodness',
     links: [
@@ -109,7 +108,7 @@ export default [
     tech: ['wordpress', 'gulp', 'less']
   },{
     name: 'Brooklyn Friends School',
-    description: `I collaborated in the development of the frontend builds that SM&KK Studios had designed for a complete refresh of the Brooklyn Friends School brand.`,
+    description: `SM&KK Studios designed a complete refresh of the Brooklyn Friends School brand.`,
     id: 5,
     slug: 'bfs',
     links: [
@@ -132,29 +131,28 @@ export default [
     featured: true,
     tech: ['wordpress', 'sass']
   },{
-    name: 'Made In Music',
-    description: `This website uses the Keystone.js CMS through Node and Express along with MongoDB. This app is currently running ECMAScript 2015 via transpilation by Babel.`,
+    name: 'Mama We Made It',
+    description: `A 'Universal' React application custom CMS. All data is sent upon inital load with the app rendering on both the server and client side.`,
     id: 6,
-    slug: 'mim',
+    slug: 'mwmi',
     links: [
       {
         name: 'Website',
-        url: 'http://madeinmusic.co',
+        url: 'http://mamawemadeit.com',
         icon: 'fa fa-chrome'
       }, {
         name: 'Github',
-        url: 'https://github.com/evturn/madeinmusic.co',
+        url: 'https://github.com/evturn/mwmi',
         icon: 'fa fa-github-square'
       }
     ],
-    thumbnail: 'mim-tn.png',
+    thumbnail: 'mwmi-tn.png',
     images: [
-      'mim-1.png',
-      'mim-2.png',
-      'mim-3.png'
+      'mwmi-1.png',
+      'mwmi-2.png'
     ],
     featured: true,
-    tech: ['node', 'backbone', 'mongodb', 'gulp', 'less']
+    tech: ['node', 'react', 'rxjs', 'redux', 'webpack', 'mongodb']
   },{
     name: 'Tectonic',
     description: `Displays a real-time list of earthquakes, including information about their locations, magnitudes, and dates. All data for the map and any earthquake comes through from the USGS. Using RxJS, a Reactive Extensions library, this stream of data is merged with another a WebSocket server that takes coordinates from earthquakes as they arrive and searches Twitter for tweet in that location.`,
@@ -179,9 +177,33 @@ export default [
     featured: true,
     tech: ['node', 'rxjs', 'web-socket', 'less', 'webpack']
   },{
+    name: 'Made In Music',
+    description: `Keystone.js, a Node based CMS, uses Express and MongoDB.`,
+    id: 8,
+    slug: 'mim',
+    links: [
+      {
+        name: 'Website',
+        url: 'http://madeinmusic.co',
+        icon: 'fa fa-chrome'
+      }, {
+        name: 'Github',
+        url: 'https://github.com/evturn/madeinmusic.co',
+        icon: 'fa fa-github-square'
+      }
+    ],
+    thumbnail: 'mim-tn.png',
+    images: [
+      'mim-1.png',
+      'mim-2.png',
+      'mim-3.png'
+    ],
+    featured: true,
+    tech: ['node', 'backbone', 'mongodb', 'gulp', 'less']
+  },{
     name: 'Pique',
     description: `Pique is an app for people who are interested in networking, collaborating, and working on projects.`,
-    id: 8,
+    id: 9,
     slug: 'pique',
     links: [
       {
@@ -202,7 +224,7 @@ export default [
   },{
     name: 'Where TO',
     description: `Search venues around you and bookmark spots. Create custom lists of places you want to remember and can reference when you want to try something new.`,
-    id: 9,
+    id: 10,
     slug: 'where-to',
     links: null,
     thumbnail: 'whereto-tn.png',
@@ -214,8 +236,8 @@ export default [
     tech: ['node', 'mongodb', 'sass']
   },{
     name: 'Ramen Buffet',
-    description: `Ramen Buffet manages multiple lists of tasks or todos. Within these lists, tasks can be sorted by importance, priority, or status.`,
-    id: 10,
+    description: `Manage multiple lists of tasks or todos. Lists can be sorted by importance, priority, or status.`,
+    id: 11,
     slug: 'ramen-buffet',
     links: [
       {
