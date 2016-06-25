9eb9a517225f1b6b3a75401e717e7df560555d54
Evan Turner <evturn@gmail.com>
2016-06-25 11:19:30 -0400

    Update video playlist actions

--- src/app/containers/Home/actions.js
+++ src/app/containers/Home/actions.js
@@ -10,72 +10,72 @@ import {
   UNMOUNT_PLAYER,
 } from './constants'
 
-const mountPlayer = player => (
-  (actions, store) => {
-
-    if (store.getState().route.locationBeforeTransitions.pathname.length > 1
-      || window.innerWidth <= 1024) {
-      return abortVideoPlayerSetup()
-    }
-
-    function abortVideoPlayerSetup() {
-      return Rx.Observable.timer(800)
-        .map(_ => ({ type: REMOVE_LOADING_INDICATOR }))
-        .startWith({ type: INITIALIZE_PLAYER_ABORT })
-    }
-
-    function videoIsPlaying(player) {
-      store.getState().video.initialized
-        ? player.playbackRate = 0.6
-        : store.dispatch(_ =>
-            Rx.Observable.of({ type: INITIALIZE_PLAYER_SUCCESS })
-          )
-
-      return Rx.Observable.timer(300)
-        .map(_ => ({ type: REMOVE_LOADING_INDICATOR }))
-        .startWith({ type: FADE_LOADING_INDICATOR })
-    }
-
-    function videoHasEnded(playlist) {
-      return Rx.Observable.of(store.getState().video.id)
-        .map(x => (
-          x === playlist.length - 1
-            ? 0
-            : x + 1
-        ))
-        .map(x => ({
-          type: PLAY_NEXT_IN_QUEUE,
-          src: playlist[x],
-          id: x
-        }))
-    }
-
-
-    return Rx.Observable.of(store.getState().video)
-      .flatMap(({ playlist }) => {
-
-        store.dispatch(actions =>
-          Rx.Observable.timer(3000)
-            .map(_ => ({ type: FADE_LOADING_INDICATOR }))
-            .takeUntil(actions.ofType(INITIALIZE_PLAYER_SUCCESS))
-            .startWith({ type: INITIALIZE_PLAYER })
-        )
-
-        return Rx.Observable.merge(
-          Rx.Observable.fromEvent(player, 'playing')
-            .flatMap(_ => videoIsPlaying(player)),
-          Rx.Observable.fromEvent(player, 'ended')
-            .flatMap(_ => videoHasEnded(playlist))
-        )
-      })
-  }
-)
-
 const unmountPlayer = _ => (
   (actions, store) => (
     Rx.Observable.of({ type: UNMOUNT_PLAYER })
   )
 )
+
+const mountPlayer = player => (
+  (actions, store) => {
+    return store.getState().route.locationBeforeTransitions.pathname.length > 1
+      || window.innerWidth <= 1024
+        ? Rx.Observable.timer(800)
+            .mapTo({ type: REMOVE_LOADING_INDICATOR })
+            .startWith({ type: INITIALIZE_PLAYER_ABORT })
+        : Rx.Observable.of(store.getState().video)
+            .flatMap(({ playlist }) => {
+              store.dispatch(startLoadingTimeout)
+
+              return Rx.Observable.merge(
+                Rx.Observable.fromEvent(player, 'playing')
+                  .map(_ => (
+                    store.getState().video.initialized
+                      ? player.playbackRate = 0.6
+                      : store.dispatch(videoHasInitialized)
+                  ))
+                  .flatMap(videoIsPlaying),
+                Rx.Observable.fromEvent(player, 'ended')
+                  .map(_ => store.getState().video.id)
+                  .map(videoHasEnded)
+                  .flatMap(fn => fn(playlist))
+              )
+            })
+  }
+)
+
+function startLoadingTimeout(actions) {
+  return Rx.Observable.timer(3000)
+    .mapTo({ type: FADE_LOADING_INDICATOR })
+    .takeUntil(actions.ofType(INITIALIZE_PLAYER_SUCCESS))
+    .startWith({ type: INITIALIZE_PLAYER })
+}
+
+function videoHasInitialized() {
+  return Rx.Observable.of({ type: INITIALIZE_PLAYER_SUCCESS })
+}
+
+function videoIsPlaying() {
+  return Rx.Observable.timer(300)
+    .mapTo({ type: REMOVE_LOADING_INDICATOR })
+    .startWith({ type: FADE_LOADING_INDICATOR })
+}
+
+function videoHasEnded(id) {
+  return playlist => (
+    Rx.Observable.of(id)
+      .map(x => (
+        x === playlist.length - 1
+          ? 0
+          : x + 1
+      ))
+      .map(x => ({
+        type: PLAY_NEXT_IN_QUEUE,
+        src: playlist[x],
+        id: x
+      }))
+  )
+}
 
 export {
   mountPlayer,
