7a28fd4cd32b0e0e9d15df35af30f0bb106cea6d
Evan Turner <evturn@gmail.com>
2016-07-20 03:30:35 -0400

    Reduce nested components in Header

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,37 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router'
-
 import Logo from 'components/Logo'
-
 import css from './style.css'
 
-const Navbar = ({ nav, children }) => (
-  <div className={css.nav}>
-    <div className={css.pages}>{
-      nav.map((x, i) => (
-        <div key={i} className={css.page}>
-          <Link
-            to={x.route}
-            children={x.name}
-          />
-        </div>
-    ))}</div>
-    {children}
-    <div className={css.diamond} />
-  </div>
-)
-
-const Header = ({ nav, ready, visible, children }) => (
+export default ({ nav, ready, visible, children }) => (
   <header className={css.root}>
-    <Navbar nav={nav}>
+    <div className={css.nav}>
+      <div className={css.pages}>
+        {nav.map(x => (
+          <div key={x.name} className={css.page}>
+            <Link
+              to={x.route}
+              children={x.name}
+            />
+          </div>
+        ))}
+      </div>
       <Logo
         ready={ready}
         visible={visible}
       />
-    </Navbar>
+      <div className={css.diamond} />
+    </div>
     <div className={css.subnav} />
     {children}
   </header>
 )
-
-export default Header
