eed1e13b6dc0094f69d91372936a63d36b5e4921
Evan Turner <evturn@gmail.com>
2016-07-27 01:23:42 -0400

    Update carousel assembler

--- src/internals/data/carousel.js
+++ src/internals/data/carousel.js
@@ -1,38 +1,26 @@
 import webProjects from './web'
 import devicons from './devicons'
 
-function assembleCarousel() {
+const nav = webProjects.reduce((acc, x) => {
+  acc.nav.push({
+    src: 'https://media3.giphy.com/media/lpydvIOdsHw0U/200_s.gif',
+    id: x.id
+  })
+  return acc
+}, [])
 
-  function assignImageSource(props) {
-    return {
-      ...props,
-      src: props.src
-    }
-  }
 
-  function reducer(acc, x) {
-    if (!acc.nav) { acc.nav = [] }
-    if (!acc.projects) { acc.projects = [] }
+const projects = webProjects.reduce((acc, x) => {
+  acc.push({
+    ...x,
+    images: x.images
+      .map((x, i) => ({
+        src: 'https://media3.giphy.com/media/lpydvIOdsHw0U/200_s.gif',
+        active: i === 0
+      })),
+    tech: x.tech.map(x => devicons.filter(y => x === y.slug))
+  })
+  return acc
+}, [])
 
-    acc.nav.push(assignImageSource({
-      src: 'https://media3.giphy.com/media/lpydvIOdsHw0U/200_s.gif',
-      id: x.id
-    }))
-    acc.projects.push({
-      ...x,
-      images: x.images
-        .map((x, i) => assignImageSource({
-          src: 'https://media3.giphy.com/media/lpydvIOdsHw0U/200_s.gif',
-          active: i === 0
-        })),
-      tech: x.tech
-        .map(x => devicons.filter(y => x === y.slug))
-    })
-
-    return acc
-  }
-
-  return webProjects.reduce(reducer, {})
-}
-
-export default assembleCarousel()
+export default { nav, projects }
