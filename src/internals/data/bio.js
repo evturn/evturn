4a854f162e7a12ad59ff9f901df8911a5127a2cb
Evan Turner <evturn@gmail.com>
2016-07-28 02:51:26 -0400

    Consolidate reduce function for bio data

--- src/internals/data/bio.js
+++ src/internals/data/bio.js
@@ -16,18 +16,11 @@ const technologies = [
   'node',
 ]
 
-function assembleBio() {
-
-  function reducer(acc, x) {
+export default {
+  copy: `As the web continues to evolve, exploring solutions and strategies for building rich applications is not only essential but provides an exciting opportunity for design innovation.`,
+  tools: technologies.reduce((acc, x) {
     return devicons
       .filter(y => x === y.slug)
       .concat(acc)
-  }
-
-  return {
-    copy: `As the web continues to evolve, exploring solutions and strategies for building rich applications is not only essential but provides an exciting opportunity for design innovation.`,
-    tools: technologies.reduce(reducer, []),
-  }
+  }, []),
 }
-
-export default assembleBio()
