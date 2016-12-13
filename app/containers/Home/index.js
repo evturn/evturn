46262bdc1b1405a2b5af5699871373145767ff32
Evan Turner <evturn@gmail.com>
2016-12-12 22:43:52 -0500

    Remove navigation component

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import HomeVideo from './HomeVideo'
-import HomeBanner from './HomeBanner'
 import LoadingIndicator from 'components/LoadingIndicator'
 import css from './style.css'
 
