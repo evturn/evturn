e89fd94dd8a3ccbe541bf9b474124570eab85ace
Evan Turner <evturn@gmail.com>
2017-01-14 20:30:49 -0500

    Move SEO into markup

--- app/components/Head/index.js
+++ app/components/Head/index.js
@@ -9,24 +9,6 @@ const Head = props => {
       titleAttributes={{itemprop: 'name', lang: 'en'}}
       title='Software Engineer'
       titleTemplate='Evan Turner | %s'
-      meta={[
-        {name: 'twitter:card',        content: 'summary'},
-        {name: 'twitter:title',       content: 'Evan Turner | Software Engineer'},
-        {name: 'twitter:image',       content: 'build/favicon-128x128.png'},
-        {name: 'twitter:image:src',   content: 'build/favicon-128x128.png'},
-        {name: 'twitter:image:alt',   content: 'An elegant stencil in black and white of a guy with a beard and shades'},
-        {name: 'twitter:site',        content: '@evturn'},
-        {name: 'twitter:url',         content: 'https://evturn.com/#/'},
-        {name: 'twitter:description', content: 'Software Engineer based in New York City developing apps for web, mobile, and emerging technologies'},
-        {property: 'og:description',  content: 'Software Engineer based in New York City developing apps for web, mobile, and emerging technologies'},
-        {property: 'og:title',        content: 'Evan Turner | Software Engineer'},
-        {property: 'og:site_name',    content: 'Evan Turner'},
-        {property: 'og:url',          content: 'https://evturn.com/#/'},
-        {property: 'og:image',        content: 'build/favicon-128x128.png'},
-        {property: 'og:image:alt',    content: 'An elegant stencil in black and white of a guy with a beard and shades'},
-        {property: 'og:type',         content: 'website'},
-        {property: 'og:locale',       content: 'en_US'},
-      ]}
       link={[
         {rel: 'apple-touch-icon',                   href: 'build/apple-touch-icon.png'},
         {rel: 'apple-touch-icon', sizes: '57x57',   href: 'build/apple-touch-icon-57x57.png'},
