38538c0411733e890fe3b83bea78022ecc4d8686
Evan Turner <ev@evturn.com>
2018-03-02 00:56:13 -0500

    Bootstrap routes with initial data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import styles from './style.css';
 import Router from 'components/Router';
+import data from './data.json';
 
 class App extends Component {
   render() {
     return (
-      <Router />
+      <Router data={data} />
     );
   }
 }
