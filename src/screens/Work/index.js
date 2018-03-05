97e5d297fdd885c8483daa4f3b1ef901f0b51c49
Evan Turner <ev@evturn.com>
2018-03-05 17:51:51 -0500

    Render work project data

--- src/screens/Work/index.js
+++ src/screens/Work/index.js
@@ -6,7 +6,7 @@ const Work = ({ data }) => {
   return (
     <div className={styles.root}>
       <div className={styles.items}>
-        {data.software.map(item =>
+        {data.work.map(item =>
           <Card 
             item={item}
             key={item.slug}/>
