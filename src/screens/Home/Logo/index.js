ecd1a879458078e6286a7928cd5b99702cc640af
Evan Turner <ev@evturn.com>
2018-03-11 14:17:02 -0400

    Add CSS classes to svg polygon elements

--- src/screens/Home/Logo/index.js
+++ src/screens/Home/Logo/index.js
@@ -2,6 +2,131 @@ import React from 'react';
 import Avatar from 'shared/Avatar';
 import styles from './style.css';
 
+const Logo = props => {
+  return (
+    <svg 
+        style={{width: '50%', height:' 50%'}}
+        viewBox="50 50 300 350">
+        <g>
+          /* Vertices */
+          <polygon points="200 75,
+                           50  150,
+                           100 150" 
+                   className={styles.s2} />
+          <polygon points="200 75,
+                           100 150,
+                           200 150" 
+                   className={styles.s3} />
+          <polygon points="200 75,
+                           200 150,
+                           300 150" 
+                   className={styles.s5} />
+          <polygon points="200 75,
+                           300 150,
+                           350 150"
+                   className={styles.s6} />
+          /* Left Diagonal */
+          <polygon points="50  150,
+                           75  225,
+                           50  300" 
+                   className={styles.s0} />
+          /* Top-Left Isosceles */
+          <polygon points="75  225,
+                           50  150,
+                           100 150" 
+                   className={styles.s1} />
+          /* Top-Left Pyramid */
+          <polygon points="150 225,
+                           100 150,
+                           200 150"
+                   className={styles.s2} />
+          /* Left-Outer-Left Diagonal */
+          <polygon points="100 150,
+                           75  225,
+                           100 300" 
+                   className={styles.s3} />
+          /* Left-Outer-Right Diagonal */
+          <polygon points="100 150,
+                           150 225,
+                           100 300" 
+                   className={styles.s3} />
+          /* Inner-Left Diagonal */
+          <polygon points="200 150,
+                           200 300,
+                           100 300" 
+                   className={styles.s3} />
+          /* Inner-Right Diagonal */
+          <polygon points="200 150,
+                           250 225,
+                           200 300" 
+                   className={styles.s3} />
+          /* Right-Outer-Left Diagonal */
+          <polygon points="300 150,
+                           250 225,
+                           300 300" 
+                   className={styles.s3} />
+          /* Right-Outer-Right Diagonal */
+          <polygon points="350 150,
+                           300 150,
+                           300 300" 
+                   className={styles.s3} />
+          /* Right Diagonal */
+          <polygon points="350 150,
+                           300 150,
+                           350 300" 
+                   className={styles.s6} />
+          /* Top-Right Pyramid */
+          <polygon points="250 225,
+                           200 150,
+                           300 150"
+                   className={styles.s4} />
+          /* Top-Right Isosceles */
+          <polygon points="325 225,
+                           300 150,
+                           350 150" 
+                   className={styles.s5} />
+          /* Bottom-Left Isosceles */
+          <polygon points="75  225,
+                           50  300,
+                           100 300" 
+                   className={styles.s1} />
+          /* Bottom-Left Pyramid */
+          <polygon points="150 225,
+                           100 300,
+                           200 300" 
+                   className={styles.s2} />
+          /* Bottom-Right Pyramid */
+          <polygon points="250 225,
+                           200 300,
+                           300 300" 
+                   className={styles.s4} />
+          /* Bottom-Right Isosceles */
+          <polygon points="325 225,
+                           300 300,
+                           350 300"
+                   className={styles.s5} />
+          /* Vertices */
+          <polygon points="200 375,
+                           50  300,
+                           100 300" 
+                   className={styles.s0} />
+          <polygon points="200 375,
+                           200 300,
+                           100 300" 
+                   className={styles.s1} />
+          <polygon points="200 375,
+                           300 300,
+                           200 300" 
+                   className={styles.s3} />
+          <polygon points="200 375,
+                           300 300,
+                           350 300"
+                   className={styles.s4} />
+        </g>
+      </svg>
+  );
+};
+
 /*
                                           _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
  .vertices                               |                             |
@@ -21,131 +146,6 @@ import styles from './style.css';
                                          | _ _ _ _ _ _ _ _ _ _ _ _ _ _ |
 
 */
-
-const Logo = props => {
-  return (
-    <svg 
-        style={{width: '50%', height:' 50%'}}
-        viewBox="50 50 300 350">
-        <g>
-          <polygon points="200 75,
-                           50  150,
-                           100 150" 
-                   fill="#4e5053" />
-          <polygon points="200 75,
-                           100 150,
-                           200 150" 
-                   fill="#67696d" />
-          <polygon points="200 75,
-                           200 150,
-                           300 150" 
-                   fill="#babac2" />
-          <polygon points="200 75,
-                           300 150,
-                           350 150"
-                   fill="#e0e0e6" />
-          
-          <polygon points="50  150,
-                           75  225,
-                           50  300" 
-                   fill="#2b2a2e"/>
-          
-          <polygon points="75  225,
-                           50  150,
-                           100 150" 
-                   fill="#3e3d42"/>
-          
-          <polygon points="150 225,
-                           100 150,
-                           200 150"
-                   fill="#4e5053"/>
-          
-          <polygon points="100 150,
-                           75  225,
-                           100 300" 
-                   fill="#67696d"/>
-          
-          <polygon points="100 150,
-                           150 225,
-                           100 300" 
-                   fill="#67696d"/>
-          
-          <polygon points="200 150,
-                           200 300,
-                           100 300" 
-                   fill="#67696d"/>
-          
-          <polygon points="200 150,
-                           250 225,
-                           200 300" 
-                   fill="#67696d"/>
-          
-          <polygon points="300 150,
-                           250 225,
-                           300 300" 
-                   fill="#67696d"/>
-
-          <polygon points="350 150,
-                           300 150,
-                           300 300" 
-                   fill="#67696d"/>
-          
-          <polygon points="350 150,
-                           300 150,
-                           350 300" 
-                   fill="#e0e0e6"/>
-
-          
-          <polygon points="250 225,
-                           200 150,
-                           300 150"
-                   fill="#7b8184"/>
-          
-          <polygon points="325 225,
-                           300 150,
-                           350 150" 
-                   fill="#babac2" />
-          
-          
-          
-          <polygon points="75  225,
-                           50  300,
-                           100 300" 
-                   fill="#3e3d42" />
-          <polygon points="150 225,
-                           100 300,
-                           200 300" 
-                   fill="#4e5053" />
-          <polygon points="250 225,
-                           200 300,
-                           300 300" 
-                   fill="#7b8184" />
-          <polygon points="325 225,
-                           300 300,
-                           350 300"
-                   fill="#babac2" />
-          
-          <polygon points="200 375,
-                           50  300,
-                           100 300" 
-                   fill="#2b2a2e" />
-          <polygon points="200 375,
-                           200 300,
-                           100 300" 
-                   fill="#3e3d42" />
-          <polygon points="200 375,
-                           300 300,
-                           200 300" 
-                   fill="#67696d" />
-          <polygon points="200 375,
-                           300 300,
-                           350 300"
-                   fill="#7b8184" />
-        </g>
-      </svg>
-  );
-};
-
 // const Logo = props => {
 //   return (
 //     <div className={styles.root}>
