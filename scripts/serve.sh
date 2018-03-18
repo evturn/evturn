83ab1560f0e9749649a43ea71f76508999c38973
Evan Turner <ev@evturn.com>
2018-03-18 16:27:34 -0400

    Add conditional deploy command to serve script

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -2,6 +2,11 @@
 
 TOOLSDIR="${PWD}/tools";
 
+if [ $# -eq 0 ]; then
+  node --experimental-modules --no-warnings "${TOOLSDIR}/server.prod.mjs";
+else
+  nohup node --experimental-modules --no-warnings "${TOOLSDIR}/server.prod.mjs" &
+fi;
+
 echo `tput setaf 2`$'\n➤  Starting production server\n'`tput sgr0`;
 
-node --experimental-modules --no-warnings "${TOOLSDIR}/server.prod.mjs";
