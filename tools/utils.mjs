d48ce887096c75e31ad8d569f08b1529502f6926
Evan Turner <ev@evturn.com>
2018-03-17 21:33:01 -0400

    Create plugin to interpolate dynamic static paths in html

--- tools/utils.mjs
+++ tools/utils.mjs
@@ -1,3 +1,39 @@
 import path from 'path';
 
-export const pathTo = (...args) => path.resolve(process.cwd(), ...args);
+const matchOperatorRe = /[|\\{}()[\]^$+*?.]/g;
+const escapeStringRegexp = str => {
+	if (typeof str !== 'string') {
+		throw new TypeError('Expected a string.');
+	}
+	return str.replace(matchOperatorRe, '\\$&');
+};
+
+class InterpolateHTMLPlugin {
+  constructor(replacements) {
+    this.replacements = replacements;
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap('InterpolateHTMLPlugin', compilation => {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap(
+        'InterpolateHTMLPlugin',
+        data => {
+          Object.keys(this.replacements).forEach(key => {
+            const value = this.replacements[key];
+            data.html = data.html.replace(
+              new RegExp('%' + escapeStringRegexp(key) + '%', 'g'),
+              value
+            );
+          });
+        }
+      );
+    });
+  }
+}
+
+const pathTo = (...args) => path.resolve(process.cwd(), ...args);
+
+export {
+  InterpolateHTMLPlugin,
+  pathTo,
+};
